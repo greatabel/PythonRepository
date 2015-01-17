db0cf7b5ae74c13323b86ccb53f04fc3a1e681d0
greatabel <myreceiver2for2github@gmail.com>
2015-01-17 21:57:22 +0800

    ch1 related

---
+++ hello_ch1.py
@@ -0,0 +1,20 @@
+print("Hello","World!")
+
+x = "blue"
+y = "green"
+z = x
+print(x,y,z)
+
+route = 866
+print(route,type(route))
+route = "North"
+print(route,type(route))
+
+print( len(("one",)) )
+print( len([1,2,3,"pause",5]))
+print(len("great"))
+
+x=["test",1,-11,"desk",12]
+print(x)
+x.append("more")
+print(x)
