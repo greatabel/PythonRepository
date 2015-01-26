a501e17d974700a3d7411f8951f02a8abf8cf983
greatabel <myreceiver2for2github@gmail.com>
2015-01-27 00:40:40 +0800

    test pdb pdb++ pyCharm

---
+++ test_build_ch2.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+print('测试中文')
+test = '1234'
+print(test)
+# import pdb 
+
+for i in range(10,20):
+	print('i='+str(i))
+
+# pdb.set_trace()
+test += '567'
+print('test build1'+test)
+print('end')
+
+
+import asyncio
+
+def hello_world(loop):
+    print('Hello World')
+    loop.stop()
+
+loop = asyncio.get_event_loop()
+
+# Schedule a call to hello_world()
+loop.call_soon(hello_world, loop)
+
+# Blocking call interrupted by loop.stop()
+loop.run_forever()
+loop.close()
+
+import sys, pdb
+sys.excepthook = lambda x,y,z: pdb.pm()
