cf423f9644c28157cdfb8527f4c4be6926438c42
greatabel <myreceiver2for2github@gmail.com>
2015-02-20 23:38:48 +0800

    doctest

--- 6OO/Shape.py
+++ 6OO/Shape.py
@@ -1,4 +1,5 @@
 import math
+import doctest
 class Point:
 	def __init__(self,x=0,y=0):
 		self.x = x
@@ -15,3 +16,58 @@ class Point:
 
 	def __str__(self):
 		return "({0.x!r},{0.y!r})".format(self)
+
+class Circle(Point):
+
+    def __init__(self, radius, x=0, y=0):
+        """A Circle
+        >>> circle = Circle(2)
+        >>> circle
+        Circle(2, 0, 0)
+        """
+        super().__init__(x, y)
+        self.radius = radius
+
+
+    def edge_distance_from_origin(self):
+        """The distance of the circle's edge from the origin
+        >>> circle = Circle(2, 3, 4)
+        >>> circle.edge_distance_from_origin()
+        3.0
+        """
+        return abs(self.distance_from_origin() - self.radius)
+
+
+    def area(self):
+        """The circle's area
+        >>> circle = Circle(10)
+        >>> a = circle.area()
+        >>> int(a)
+        314
+        """
+        return math.pi * (self.radius ** 2)
+
+
+    def circumference(self):
+        """The circle's circumference
+        >>> circle = Circle(3)
+        >>> d = circle.circumference()
+        >>> int(d)
+        18
+        """
+        return 2 * math.pi * self.radius
+
+
+    def __eq__(self, other):
+        return self.radius == other.radius and super().__eq__(other)
+
+
+    def __repr__(self):
+        return "Circle({0.radius!r}, {0.x!r}, {0.y!r})".format(self)
+
+
+    def __str__(self):
+        return repr(self)
+
+if __name__ == "__main__":
+    doctest.testmod()
