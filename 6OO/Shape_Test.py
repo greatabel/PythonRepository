b4b3e0323649dfc1b1a7cd42ddc59dce98440677
greatabel <myreceiver2for2github@gmail.com>
2015-02-21 23:42:27 +0800

    test

--- 6OO/Shape_Test.py
+++ 6OO/Shape_Test.py
@@ -8,3 +8,6 @@ print("b.distance_from_origin()=",b.distance_from_origin())
 b.x = -19
 print(str(b))
 print( a==b, a!=b)
+
+c = Shape.Circle(5,28,45)
+print("c.distance_from_origin()=",c.distance_from_origin())
