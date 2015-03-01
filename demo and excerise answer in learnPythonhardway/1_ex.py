fd9980a2d501b2f0277e2cf7f37fd93d589a0eab
greatabel <myreceiver2for2github@gmail.com>
2015-03-01 16:04:13 +0800

    hard way

---
+++ demo and excerise answer in learnPythonhardway/1_ex.py
@@ -0,0 +1,7 @@
+# print "Hello World!" http://learnpythonthehardway.org/book/ex1.html
+print "Hello Again" 
+print "I like typing this."
+print "This is fun."
+print 'Yay! Printing.'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
---
+++ demo and excerise answer in learnPythonhardway/44alter_befor_after.py
@@ -0,0 +1,15 @@
+class Parent(object):
+  def altered(self):
+   print "Parent altered()"
+
+class Child(Parent):
+  def altered(self):
+    print "Child,before parent altered()"
+    super(Child,self).altered()
+    print "Child ,after parent altered()"
+
+dad=Parent()
+son=Child()
+
+dad.altered()
+son.altered()
