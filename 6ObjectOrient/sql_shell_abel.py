245b2c834df2e3af83983cfaea26131652180e25
greatabel <myreceiver2for2github@gmail.com>
2015-03-07 22:51:47 +0800

    sqlite3

---
+++ 6ObjectOrient/sql_shell_abel.py
@@ -0,0 +1,52 @@
+# A minimal SQLite shell for experimentsimport sqlite3
+
+#使用：select * from stocks;
+
+import sqlite3
+con = sqlite3.connect(":memory:")
+con.isolation_level = None
+cur = con.cursor()
+
+
+#---------------------------------
+c = con.cursor()
+
+# Create table
+c.execute('''CREATE TABLE stocks
+             (date text, trans text, symbol text, qty real, price real)''')
+
+# Insert a row of data
+c.execute("INSERT INTO stocks VALUES ('2006-01-05','BUY','RHAT',100,35.14)")
+
+# Save (commit) the changes
+con.commit()
+
+# We can also close the connection if we are done with it.
+# Just be sure any changes have been committed or they will be lost.
+
+#---------------------------------
+
+
+
+buffer = ""
+
+print("Enter your SQL commands to execute in sqlite3.")
+print("Enter a blank line to exit.")
+
+while True:
+    line = input()
+    if line == "":
+        break
+    buffer += line
+    if sqlite3.complete_statement(buffer):
+        try:
+            buffer = buffer.strip()
+            cur.execute(buffer)
+
+            if buffer.lstrip().upper().startswith("SELECT"):
+                print(cur.fetchall())
+        except sqlite3.Error as e:
+            print("An error occurred:", e.args[0])
+        buffer = ""
+
+con.close()
