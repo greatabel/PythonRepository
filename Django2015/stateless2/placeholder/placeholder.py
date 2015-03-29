7da2332b59cc96208db7a6a7da10cf94350c5d1e
greatabel <myreceiver2for2github@gmail.com>
2015-03-30 00:54:28 +0800

    stateless2

---
+++ Django2015/stateless2/placeholder/placeholder.py
@@ -0,0 +1,47 @@
+import os
+import sys
+
+from django.conf import settings
+
+DEBUG = os.environ.get('DEBUG', 'on') == 'on'
+
+SECRET_KEY = os.environ.get('SECRET_KEY', 'renqh)05f(74v^91=3yv5+ie*k4wvo)c4s&-#=1)nu2@m)0uf#')
+
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
+
+settings.configure(
+    DEBUG=DEBUG,
+    SECRET_KEY=SECRET_KEY,
+    ALLOWED_HOSTS=ALLOWED_HOSTS,
+    ROOT_URLCONF=__name__,
+    MIDDLEWARE_CLASSES=(
+        'django.middleware.common.CommonMiddleware',
+        'django.middleware.csrf.CsrfViewMiddleware',
+        'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    ),
+)
+
+from django.conf.urls import url
+from django.core.wsgi import get_wsgi_application
+from django.http import HttpResponse
+
+def placeholder(request, width, height):
+    #TODO: rest of the view will go here
+    return HttpResponse('OK')
+
+def index(request):
+    return HttpResponse('Hello World from placeholder!')
+
+
+urlpatterns = (
+    url(r'^$', index),
+)
+
+
+application = get_wsgi_application()
+
+
+if __name__ == "__main__":
+    from django.core.management import execute_from_command_line
+
+    execute_from_command_line(sys.argv)
