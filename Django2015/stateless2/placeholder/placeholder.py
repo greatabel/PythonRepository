6f10136629fabc23638baf9716219da0c06b91be
greatabel <myreceiver2for2github@gmail.com>
2015-03-30 09:58:11 +0800

    stateless badrequest

--- Django2015/stateless2/placeholder/placeholder.py
+++ Django2015/stateless2/placeholder/placeholder.py
@@ -21,21 +21,37 @@ settings.configure(
     ),
 )
 
+from django import forms
 from django.conf.urls import url
 from django.core.wsgi import get_wsgi_application
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseBadRequest
+
+class ImageForm(forms.Form):
+    """Form to validate requested placeholder image."""
+    height = forms.IntegerField(min_value=1, max_value=2000)
+    width  = forms.IntegerField(min_value=1, max_value=2000)
 
 def placeholder(request, width, height):
     #TODO: rest of the view will go here
-    return HttpResponse('OK')
+    form = ImageForm({'height':height,'width':width})
+    if form.is_valid():
+        height = form.cleaned_data['height']
+        width  = form.cleaned_data['width']
+        return HttpResponse('OK')
+    else:
+        return HttpResponseBadRequest('Invalid Image Request')
+
 
 def index(request):
     return HttpResponse('Hello World from placeholder!')
 
 
 urlpatterns = (
-    url(r'^$', index),
+    url(r'^image/(?P<width>[0-9]+)x(?P<height>[0-9]+)/$', placeholder,
+        name='placeholder'),
+    url(r'^$', index, name='homepage'),
 )
+
 
 
 application = get_wsgi_application()
