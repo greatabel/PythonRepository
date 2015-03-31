1d0435ace011f8df0f3278e0a6da10ff510bcd5e
greatabel <myreceiver2for2github@gmail.com>
2015-03-31 22:51:19 +0800

    dynamic

--- Django2015/sitegenerator3/sitebuilder/views.py
+++ Django2015/sitegenerator3/sitebuilder/views.py
@@ -1,9 +1,13 @@
+import json
 import os
 
 from django.conf import  settings
 from django.http import  Http404
 from django.shortcuts import render
-from django.template import Template
+from django.template import Template, Context
+from django.template.loader_tags import BlockNode
+from django.utils._os import safe_join
+
 
 from django.utils._os import safe_join
 
@@ -19,6 +23,13 @@ def get_page_or_404(name):
     with open(file_path, 'r') as f:
         page = Template(f.read())
 
+    meta = None
+    for i, node in enumerate(list(page.nodelist)):
+        if isinstance(node,BlockNode) and node.name == 'context':
+            meta = page.nodelist.pop(i)
+            break
+    page._meta = meta
+
     return page
 
 def page(request, slug='index'):
@@ -28,4 +39,8 @@ def page(request, slug='index'):
         'slug':slug,
         'page': page,
     }
+    if page._meta is not None:
+    	meta = page._meta.render(Context())
+    	extra_context = json.loads(meta)
+    	context.update(extra_context)
     return render(request, 'page.html',context)
