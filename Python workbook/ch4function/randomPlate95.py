d2282ceb474128bb255ed47b803b7180485c8525
greatabel <myreceiver2for2github@gmail.com>
2015-06-11 10:42:13 +0800

    change

---
+++ Python workbook/ch4function/randomPlate95.py
@@ -0,0 +1,35 @@
+from random import randint
+import string, random
+
+SHORTEST = 7
+LONGEST = 10
+MIN_ASCII = 33
+MAX_ASCII = 126
+
+def randomPlate():
+	
+	result = ""
+	# for i in range(3):
+	# 	print(ord('A'),ord('z'))
+	# 	randomChar = chr(randint(ord('A'),ord('z')))
+
+	# 	print('randomChar=',randomChar)
+	# 	result = result + randomChar
+	result = ''.join(random.choice(string.ascii_lowercase) for x in range(3))
+	myintrange = 0
+	myrandomtype = randint(0,1)
+	# print('myrandomtype=',myrandomtype)
+	if myrandomtype%2==0:
+		myintrange = 3
+	else:
+		myintrange = 4
+	for j in range(myintrange):
+		randomNum = str(randint(0,9))
+		result = result + randomNum
+	return result
+
+def main():
+	print("Your random pass is:", randomPlate())
+
+if __name__ == "__main__":
+	main()
