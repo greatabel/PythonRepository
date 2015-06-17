c45933909ce2cd698a2d42723ced88a9e36e5fc4
greatabel <myreceiver2for2github@gmail.com>
2015-06-17 15:59:17 +0800

    largest smallest N

---
+++ PythonCookbook/ch1Data Structures and Algorithms/largestNnumbers4.py
@@ -0,0 +1,22 @@
+import heapq
+def main():
+
+    nums = [1, 8, 2, 23, 7, -4, 18, 23, 42, 37, 2] 
+    print(heapq.nlargest(3, nums)) # Prints [42, 37, 23] 
+    print(heapq.nsmallest(3, nums)) # Prints [-4, 1, 2]
+
+    print('-----------demo2---------')
+    portfolio = [
+       {'name': 'IBM', 'shares': 100, 'price': 91.1},
+       {'name': 'AAPL', 'shares': 50, 'price': 543.22},
+       {'name': 'FB', 'shares': 200, 'price': 21.09},
+       {'name': 'HPQ', 'shares': 35, 'price': 31.75},
+       {'name': 'YHOO', 'shares': 45, 'price': 16.35},
+       {'name': 'ACME', 'shares': 75, 'price': 115.65}
+    ]
+    cheap = heapq.nsmallest(3, portfolio, key=lambda s: s['price'])
+    expensive = heapq.nlargest(3, portfolio, key=lambda s: s['price'])
+    print(cheap,'\n\n',expensive)
+
+if __name__ == '__main__':
+    main()
