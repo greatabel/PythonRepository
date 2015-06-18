71d74ddedae02853aa507523506e051ebc60d229
greatabel <myreceiver2for2github@gmail.com>
2015-06-18 23:16:12 +0800

    filter

--- GettingStartedWithDjango/Work_manager/TasksManager/views.py
+++ GettingStartedWithDjango/Work_manager/TasksManager/views.py
@@ -3,8 +3,9 @@ from django.http import HttpResponse
 # Create your views here.
 def index (request) :
     # return HttpResponse ("Hello world!" )
-    my_variable = "abel"
+    my_variable = "Hello world from abel"
     years_old = 29
+    years_test = 1
     array_city_capitals = {"Paris","London"}
-    returnV = {"my_var": my_variable,"years": years_old,"array_cities": array_city_capitals}
+    returnV = {"my_var": my_variable,"years_test":years_test,"years": years_old,"array_cities": array_city_capitals}
     return render(request, 'TasksManager/index.html', returnV)
