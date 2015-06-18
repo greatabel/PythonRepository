2f3e0830c704bb644e5cbe92cb0d7bfaf27b5d25
greatabel <myreceiver2for2github@gmail.com>
2015-06-18 23:53:13 +0800

    static

--- GettingStartedWithDjango/Work_manager/TasksManager/urls.py
+++ GettingStartedWithDjango/Work_manager/TasksManager/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^connections$', views.connections, name='connections'),
+
 ]
--- GettingStartedWithDjango/Work_manager/TasksManager/views.py
+++ GettingStartedWithDjango/Work_manager/TasksManager/views.py
@@ -4,8 +4,14 @@ from django.http import HttpResponse
 def index (request) :
     # return HttpResponse ("Hello world!" )
     my_variable = "Hello world from abel"
+    my_variable1 = "Hello world from abel\n test from abel"
     years_old = 29
     years_test = 1
     array_city_capitals = {"Paris","London"}
-    returnV = {"my_var": my_variable,"years_test":years_test,"years": years_old,"array_cities": array_city_capitals}
+    returnV = {"my_variable1":my_variable1,"my_var": my_variable,"years_test":years_test,"years": years_old,"array_cities": array_city_capitals}
     return render(request, 'TasksManager/index.html', returnV)
+
+def connections(request):
+    import random
+    i = random.randint(0,100)
+    return render(request, 'TasksManager/connections.html',{"i":i})
