1496e58dc7955ac716dffa3de4a200ca6d8ad4e5
greatabel <myreceiver2for2github@gmail.com>
2015-06-18 18:10:04 +0800

    package test

---
+++ DjangoTutorial/django-polls/polls/admin.py
@@ -0,0 +1,26 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Choice, Question
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+        
+class QuestionAdmin(admin.ModelAdmin):
+
+
+    # fields = ['pub_date', 'question_text']
+    fieldsets = [
+    (None, {'fields':['question_text']}),
+    ('Date information', {'fields':['pub_date'],
+        'classes':['collapse']})
+    ]
+    inlines = [ChoiceInline]
+
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+
+    list_filter = ['pub_date']
+
+admin.site.register(Question, QuestionAdmin)
+# admin.site.register(Choice)
