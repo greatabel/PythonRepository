93d611270588e549480476247f4ae588e770fab9
greatabel <myreceiver2for2github@gmail.com>
2015-06-22 11:25:11 +0800

    ch4

---
+++ PythonCookbook/ch4IteratorsAndGenerators/consumeIterator41.py
@@ -0,0 +1,29 @@
+from collections import OrderedDict
+
+import colorama
+from colorama import Fore, Back, Style
+
+from datetime import timedelta, datetime,date
+import calendar
+
+
+
+def main():
+    with open('passwd.txt') as f:
+        try:
+            while True:
+                line = next(f)
+                print(line, end='')
+        except StopIteration:
+            pass
+            
+    
+
+    
+    
+
+
+
+if __name__ == '__main__':
+    main()
+
