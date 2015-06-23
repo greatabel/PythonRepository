9317c714db6aa9ff8b2406bdaeae4a60a3bb3852
greatabel <myreceiver2for2github@gmail.com>
2015-06-24 00:27:44 +0800

    enumerate

--- PythonCookbook/ch4IteratorsAndGenerators/iterateOverIndexValue410.py
+++ PythonCookbook/ch4IteratorsAndGenerators/iterateOverIndexValue410.py
@@ -12,14 +12,43 @@ def testA():
     for index, val in enumerate(items,3):
         print(index,val)
 
+def testB():
+    parse_data('passwdA.txt')
+
+def testC():
+    import collections
+    word_summary = collections.defaultdict(list)
+    with open('passwdA.txt','r') as f:
+        lines = f.readlines()
+
+    for idx, line in enumerate(lines):
+        #当前行的单词
+        words = [w.strip().lower() for w in line.split()]
+        for word in words:
+            word_summary[word].append(idx)
+
+    print(word_summary)
+
+def parse_data(filename):
+    with open(filename, 'rt') as f:
+        for lineno, line in enumerate(f,1):
+            fields = line.split(',')
+            print('len=',len(fields))
+            try:
+                count = int(fields[1])
+            except ValueError as e:
+                print('line {} : Parse error : {}'.format(lineno,e))
+            except IndexError as ie:
+                print('line {} : my IndexError error : {}'.format(lineno,ie))
 
 
 def main():
     print(Back.GREEN + 'and with a green background'+Back.RESET )
-    
+    testB()
     print(Back.RED + 'and with a red background'+Back.RESET )
     testA()
-    
+    print(Back.YELLOW + 'and with a yellow background'+Back.RESET )
+    testC()
     
     
 
