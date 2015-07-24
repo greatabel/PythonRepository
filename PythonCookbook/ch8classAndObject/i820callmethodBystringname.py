3bdebffe0b5168b0bdb2bc327dccad2b5e4851fb
greatabel <myreceiver2for2github@gmail.com>
2015-07-24 14:58:13 +0800

    methodcall

--- PythonCookbook/ch8classAndObject/i820callmethodBystringname.py
+++ PythonCookbook/ch8classAndObject/i820callmethodBystringname.py
@@ -14,3 +14,21 @@ if __name__ == '__main__':
     p = Point(3, 4)
     d = getattr(p,'distance')(0,0)
     print('d=',d)
+
+    import operator
+    caller = operator.methodcaller('distance',0,0)(p)
+    print('d2=',caller)
+
+    points = [
+    Point(1, 2),
+    Point(3, 0),
+    Point(10, -3),
+    Point(-5, -7),
+    Point(-1, 8),
+    Point(3, 2)
+    ]
+
+    # Sort by distance from origin (0, 0)
+    points.sort(key=operator.methodcaller('distance', 0, 0))
+    for p in points:
+        print(p)
