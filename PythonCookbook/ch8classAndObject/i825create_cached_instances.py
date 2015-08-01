86a17e03d5ee6e61718f7a9161dc2fc68391a97a
greatabel <myreceiver2for2github@gmail.com>
2015-08-01 23:00:08 +0800

    cached

---
+++ PythonCookbook/ch8classAndObject/i825create_cached_instances.py
@@ -0,0 +1,21 @@
+class Spam:
+    def __init__(self, name):
+        self.name = name
+
+import weakref
+_spam_cache = weakref.WeakValueDictionary()
+
+def get_spam(name):
+    if name not in _spam_cache:
+        s = Spam(name)
+        _spam_cache[name] = s
+    else:
+        s = _spam_cache[name]
+    return s
+
+if __name__ == '__main__':
+    a = get_spam('foo')
+    b = get_spam('bar')
+    print('a is b:', a is b)
+    c = get_spam('foo')
+    print('a is c:', a is c)
