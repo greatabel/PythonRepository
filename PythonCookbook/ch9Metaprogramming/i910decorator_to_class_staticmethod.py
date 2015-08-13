fca95e40109ee71be9b30455ce00f8ae43b6376e
greatabel <myreceiver2for2github@gmail.com>
2015-08-13 12:12:37 +0800

    decorator to class & static method

---
+++ PythonCookbook/ch9Metaprogramming/i910decorator_to_class_staticmethod.py
@@ -0,0 +1,42 @@
+import time
+from functools import wraps
+
+def timethis(func):
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        r = func(*args, **kwargs)
+        end = time.time()
+        print(end-start)
+        return r
+
+    return wrapper
+
+
+class Spam:
+    @timethis
+    def instance_method(self,n):
+        print(self, n,"in instance_method")
+        while n > 0:
+            n -= 1
+
+    @classmethod
+    @timethis
+    def class_method(cls,n):
+        print(cls, n, "in class_method")
+        while  n > 0:
+            n -= 1
+
+    @staticmethod
+    @timethis
+    def static_method(n):
+        print(n, "static_method")
+        while n > 0:
+            n -= 1
+
+if __name__ == "__main__":
+    s = Spam()
+    s.instance_method(1000)
+    Spam.class_method(10000)
+    Spam.static_method(100000)
