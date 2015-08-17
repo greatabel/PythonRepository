ad4c28242973c0052295d09449e943fb53bcbed4
greatabel <myreceiver2for2github@gmail.com>
2015-08-17 11:55:22 +0800

    conventions

---
+++ PythonCookbook/ch9Metaprogramming/i917code_conventions.py
@@ -0,0 +1,18 @@
+class NoMixedCaseMeta(type):
+    def __new__(cls, clsname, bases, clsdict):
+        for name in clsdict:
+            if name.lower() != name:
+                raise TypeError('Bad attribute name:' + name)
+        return super().__new__(cls, clsname, bases, clsdict)
+
+class Root(metaclass=NoMixedCaseMeta):
+    pass
+
+class A(Root):
+    def foo_bar(self):
+            pass
+
+print("** about to generate a TypeError")
+class B(Root):
+    def fooBar(self):
+        pass
