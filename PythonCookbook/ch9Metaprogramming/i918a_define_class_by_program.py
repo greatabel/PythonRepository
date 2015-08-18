9a9f25ac3af6724f46edbd900bc3aeb9e2a1e1ae
greatabel <myreceiver2for2github@gmail.com>
2015-08-18 15:07:32 +0800

    other

---
+++ PythonCookbook/ch9Metaprogramming/i918a_define_class_by_program.py
@@ -0,0 +1,25 @@
+def __init__(self, name, shares, price):
+    self.name = name
+    self.shares = shares
+    self.price = price
+
+def __repr__(self):
+    return '## {0}, {1}, {2} #!'.format(self.name, self.shares, self.price)
+
+def cost(self):
+    return self.shares * self.price
+
+cls_dict = {
+    '__init__': __init__,
+    '__repr__': __repr__,
+    'cost':cost,
+}
+
+import types
+import abc
+Stock = types.new_class('Stock',(),{'metaclass': abc.ABCMeta}, lambda ns: ns.update(cls_dict))
+
+if __name__ == '__main__':
+    s = Stock('APPLE', 4,25)
+    print(s,type(Stock))
+    print('s.cost=',s.cost())
