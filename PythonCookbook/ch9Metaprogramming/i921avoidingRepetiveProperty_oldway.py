8c04fdbf0e2a10a150b9b45bfc8a9700c525ba5d
greatabel <myreceiver2for2github@gmail.com>
2015-08-21 16:50:39 +0800

    avoid property repeat

---
+++ PythonCookbook/ch9Metaprogramming/i921avoidingRepetiveProperty_oldway.py
@@ -0,0 +1,32 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str):
+            raise TypeError('name must be a string')
+        self._name = value
+
+    @property
+    def age(self):
+        return self._age
+
+    @age.setter
+    def age(self, value):
+        if not isinstance(value, int):
+            raise TypeError('age must be an int')
+        self._age = value
+
+if __name__ == '__main__':
+    p = Person('Abel', 28)
+    p.name = 'Greatabel'
+    print(p,p.name,p.age)
+    
+
+    
