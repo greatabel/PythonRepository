b31bf0d381aa211e0dd8cac7b16c030722e5b1cf
greatabel <myreceiver2for2github@gmail.com>
2015-09-04 17:59:40 +0800

    distributing

---
+++ PythonCookbook/ch10ModulesAndPackages/i15distributing_package/readme.txt
@@ -0,0 +1,3 @@
+read.me:
+调用生成方法
+python3 setup.py sdist
---
+++ PythonCookbook/ch10ModulesAndPackages/i15distributing_package/setup.py
@@ -0,0 +1,10 @@
+from distutils.core  import setup
+
+setup(
+    name='projectname',
+    version='1.0',
+    author='Abel',
+    author_email='greatabel1@126.com',
+    url='https://github.com/greatabel/PythonRepository',
+    packages=['projectname','projectname.utils']
+    )
