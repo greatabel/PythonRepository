7061e0e35a2d76d247aa0455aa18ec7733e8c4f4
greatabel <myreceiver2for2github@gmail.com>
2015-09-12 15:04:46 +0800

    tcp server

---
+++ PythonCookbook/ch11Network_and_WebProgramming/2tcp_server/1server.py
@@ -0,0 +1,15 @@
+from SocketServer import BaseRequestHandler, TCPServer
+
+class EchoHandler(BaseRequestHandler):
+    def handler(self):
+        print('Got connection from', self.client_address)
+        while True:
+            msg = self.request.recv(8192)
+            if not msg:
+                break
+            self.request.send(msg)
+
+if __name__ == '__main__':
+    serv = TCPServer(('', 20000), EchoHandler) 
+    serv.serve_forever()
+
