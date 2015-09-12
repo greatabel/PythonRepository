2da97e41756b42c0b775c53c96f0e639ea06f6bd
greatabel <myreceiver2for2github@gmail.com>
2015-09-12 15:52:05 +0800

    thread UDP

---
+++ PythonCookbook/ch11Network_and_WebProgramming/3udp_server/thread_udp_server.py
@@ -0,0 +1,14 @@
+from socketserver import BaseRequestHandler, UDPServer,ThreadingUDPServer
+import time
+
+class TimeHandler(BaseRequestHandler):
+    def handle(self):
+        print('Got connection from', self.client_address)
+        msg, sock = self.request
+        resp = time.ctime()
+        sock.sendto(resp.encode('ascii'), self.client_address)
+
+
+if __name__ == '__main__':
+    serv = ThreadingUDPServer(('',20000),TimeHandler)
+    serv.serve_forever()
