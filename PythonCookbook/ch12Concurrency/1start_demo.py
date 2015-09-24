9b8f96a9d2e09f045073f5c47939577af51fcfc7
greatabel <myreceiver2for2github@gmail.com>
2015-09-24 11:23:37 +0800

    demo

---
+++ PythonCookbook/ch12Concurrency/1start_demo.py
@@ -0,0 +1,17 @@
+import time
+
+def countdown(n):
+    while n > 0:
+        print('T-minus', n)
+        n -= 1
+        time.sleep(0.5)
+
+from threading import Thread
+t = Thread(target=countdown, args=(10,))
+t.start()
+
+# You can query a thread instance to see if it’s still running:
+if t.is_alive(): 
+    print('Still running')
+else: 
+    print('Completed')
