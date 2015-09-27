31bdbc7f70f28c71695e912683adf18d985e0a20
greatabel <myreceiver2for2github@gmail.com>
2015-09-27 17:51:05 +0800

    communicate

--- PythonCookbook/ch12Concurrency/3communicate_between_threads/i3communicate.py
+++ PythonCookbook/ch12Concurrency/3communicate_between_threads/i3communicate.py
@@ -2,3 +2,34 @@ from queue import Queue
 from threading import Thread
 import time
 
+def producer(out_q):
+    n = 10
+    while n > 0:
+        out_q.put(n)
+        time.sleep(2)
+        n -= 1
+
+        out_q.put(_sentinel)
+
+
+def consumer(in_q):
+    while True:
+
+        data = in_q.get()
+
+        if data is _sentinel:
+            in_q.put(_sentinel)
+            break
+
+        print('Got:', data)
+
+    print('consumer shutting down')
+
+if __name__ == '__main__':
+    q = Queue()
+    t1 = Thread(target=consumer, args=(q,))
+    t2 = Thread(target=producer, args=(q,))
+    t1.start()
+    t2.start()
+    t1.join()
+    t2.join()
