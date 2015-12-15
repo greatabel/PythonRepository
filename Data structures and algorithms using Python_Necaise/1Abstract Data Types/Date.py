9f7324b3f1726ea6e065376a0d241e88533b3378
greatabel <myreceiver2for2github@gmail.com>
2015-12-15 23:48:38 +0800

    change

--- Data structures and algorithms using Python_Necaise/1Abstract Data Types/Date.py
+++ Data structures and algorithms using Python_Necaise/1Abstract Data Types/Date.py
@@ -2,4 +2,7 @@ class Date :
 
     def __init__(self, month, day, year):
         self._julianDay = 0
+        assert self._isValidGregorian( month, day, year ), \
+                "Invalid Gregorian date"
+        
         
