3b0b664b80a9f9f6d3dd18f4cc4e97f00d2b5784
greatabel <myreceiver2for2github@gmail.com>
2015-12-16 12:16:20 +0800

    offical

---
+++ Data structures and algorithms using Python_Necaise/ch1Abstract Data Types/studentfile.py
@@ -0,0 +1,29 @@
+class StudentFileReader :
+# Create a new student reader instance.
+def __init__( self, inputSrc ): self._inputSrc = inputSrc self._inputFile = None
+   # Open a connection to the input file.
+def open( self ):
+self._inputFile = open( self._inputSrc, "r" )
+   # Close the connection to the input file.
+def close( self ): self._inputFile.close() self._inputFile = None
+# Extract all student records and store them in a list.
+def fetchAll( self ): theRecords = list()
+student = self.fetchRecord()
+while student != None : theRecords.append( student ) student = self.fetchRecord()
+return theRecords
+   # Extract the next student record from the file.
+def fetchRecord( self ):
+# Read the first line of the record.
+line = self._inputFile.readline() if line == "" :
+return None
+# If there is another record, create a storage object and fill it.
+student = StudentRecord()
+student.idNum = int( line )
+student.firstName = self._inputFile.readline().rstrip() student.lastName = self._inputFile.readline().rstrip() student.classCode = int( self._inputFile.readline() ) student.gpa = float( self._inputFile.readline() ) return student
+# Storage class used for an individual student record.
+class StudentRecord : def __init__( self ):
+    self.idNum = 0
+    self.firstName = None
+    self.lastName = None
+    self.classCode = 0
+    self.gpa = 0.0
