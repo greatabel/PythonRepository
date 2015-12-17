c93b3b0237819647060b9025c45cd263f8fe3c93
greatabel <myreceiver2for2github@gmail.com>
2015-12-17 11:54:18 +0800

    array demo

--- Data structures and algorithms using Python_Necaise/ch2Array/array.py
+++ Data structures and algorithms using Python_Necaise/ch2Array/array.py
@@ -2,15 +2,15 @@ import ctypes
 
 class Array :
 # Creates an array with size elements.
-def __init__( self, size ):
-    assert size > 0, "Array size must be > 0" 
-    self._size = size
-     # Create the array structure using the ctypes module.
-    PyArrayType = ctypes.py_object * size
-    self._elements = PyArrayType()
-     # Initialize each element.
-    self.clear( None )
-   # Returns the size of the array.
+    def __init__( self, size ):
+        assert size > 0, "Array size must be > 0" 
+        self._size = size
+         # Create the array structure using the ctypes module.
+        PyArrayType = ctypes.py_object * size
+        self._elements = PyArrayType()
+         # Initialize each element.
+        self.clear( None )
+       # Returns the size of the array.
     def __len__( self ): 
         return self._size
        # Gets the contents of the index element.
@@ -19,8 +19,8 @@ def __init__( self, size ):
         return self._elements[ index ]
     # Puts the value in the array element at index position.
     def __setitem__( self, index, value ):
-    assert index >= 0 and index < len(self), "Array subscript out of range" 
-    self._elements[ index ] = value
+        assert index >= 0 and index < len(self), "Array subscript out of range" 
+        self._elements[ index ] = value
     # Clears the array by setting each element to the given value.
     def clear( self, value ):
         for i in range( len(self) ) :
