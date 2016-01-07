0d9f0a3b21d3f4cd108b9c511e095cbf4d4b6e8e
greatabel <myreceiver2for2github@gmail.com>
2016-01-07 23:29:11 +0800

    generator as alternative

---
+++ PythonCookbook/ch12Concurrency/12generator_as_alternative_to_threads/client_test.py
@@ -0,0 +1,12 @@
+from socket import socket, AF_INET, SOCK_STREAM
+s = socket(AF_INET, SOCK_STREAM)
+s.connect(('127.0.0.1', 16000))
+
+s.send(b'Hello\nOne\nTwo')
+
+print('-'*10)
+
+resp = s.recv(8192)
+print('Response:', resp)
+s.close()
+
