b1d6242a2ae2050be6fbc708d2db7f2f7bd1c30a
greatabel <myreceiver2for2github@gmail.com>
2016-01-16 17:55:07 +0800

    decorator on atexit

--- PythonCookbook/ch12Concurrency/14launching_a_daemon_process_on_unix/review os module/5atexit.py
+++ PythonCookbook/ch12Concurrency/14launching_a_daemon_process_on_unix/review os module/5atexit.py
@@ -1,9 +1,16 @@
+# https://docs.python.org/3/library/atexit.html
+
+import atexit
+
 def goodbye(name, adjective):
     print('goodbye, %s, it was %s to meet you.' %(name, adjective) )
 
-import atexit
+@atexit.register
+def test2():
+    print("In test2!")
+
+
+
 atexit.register(goodbye,'Great','Abel')
-
-
 # or
 atexit.register(goodbye, adjective='who-send', name='Who-receive')
