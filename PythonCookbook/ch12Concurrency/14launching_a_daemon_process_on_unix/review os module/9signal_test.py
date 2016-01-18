1392115993ab528ee24d6b9e995c6e8d6f9145f1
greatabel <myreceiver2for2github@gmail.com>
2016-01-18 23:51:02 +0800

    change

--- PythonCookbook/ch12Concurrency/14launching_a_daemon_process_on_unix/review os module/9signal_test.py
+++ PythonCookbook/ch12Concurrency/14launching_a_daemon_process_on_unix/review os module/9signal_test.py
@@ -7,17 +7,11 @@ import time
 def receive_signal(signum, stack):#接收信号是通过建立一个回调函数，称为一个信号处理 ，信号发生时被调用
     print('Received:', signum)
 
+if __name__ == "__main__":
 
-
-signal.signal(signal.SIGUSR1, receive_signal) 
-signal.signal(signal.SIGUSR2, receive_signal)
-
-
-
-print('My PID is:', os.getpid())
-
-
-
-while True:
-    print('Waiting...')
-    time.sleep(3) #每次暂停3秒钟。 当一个信号到来时，呼叫中断唤醒并用receive_signal做信号处理
+    signal.signal(signal.SIGUSR1, receive_signal) 
+    signal.signal(signal.SIGUSR2, receive_signal)
+    print('My PID is:', os.getpid())
+    while True:
+        print('Waiting...')
+        time.sleep(3) #每次暂停3秒钟。 当一个信号到来时，呼叫中断唤醒并用receive_signal做信号处理
