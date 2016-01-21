0b92f20cee7ffda48d23238c33e53c275d3ee0c7
greatabel <myreceiver2for2github@gmail.com>
2016-01-21 22:08:39 +0800

    use bash

--- PythonCookbook/ch13Utility scripting and system admin/6shell_with_bash.py
+++ PythonCookbook/ch13Utility scripting and system admin/6shell_with_bash.py
@@ -2,9 +2,10 @@ import subprocess
 
 if __name__ == "__main__":
     try:
-        out_bytes = subprocess.check_output(['netstat', '-la'])
+        out_bytes = subprocess.check_output('ls -la', shell=True)
+        
         out_text = out_bytes.decode('utf-8')
-        print("###",out_text)
+        print("####\n"*3,out_text)
     except subprocess.CalledProcessError as e:
         print('It did not work. Reason:', e)
         print('Exitcode:', e.returncode)
