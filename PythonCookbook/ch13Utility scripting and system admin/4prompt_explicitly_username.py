08474d87190c317c99e5c2e8cb90d3d805f074e4
greatabel <myreceiver2for2github@gmail.com>
2016-01-21 17:59:02 +0800

    prompt explicitly

---
+++ PythonCookbook/ch13Utility scripting and system admin/4prompt_explicitly_username.py
@@ -0,0 +1,22 @@
+import getpass
+
+def  svc_login(user, passwd):
+    if user!="" and passwd!="":
+        return True
+    else:
+        return False
+
+
+def main():
+    user = input('Enter your username ###:')
+    passwd = getpass.getpass()
+    print('User:',user)
+    print('Password:', passwd)
+    
+    if svc_login(user, passwd): 
+        print('Yay!')
+    else: 
+        print('Boo!')
+
+if __name__ == "__main__":
+    main()
