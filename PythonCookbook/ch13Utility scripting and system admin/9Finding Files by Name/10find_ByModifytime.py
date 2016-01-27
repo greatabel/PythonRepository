723e9a039a37afb0c176c58653302927fbc5312d
greatabel <myreceiver2for2github@gmail.com>
2016-01-27 23:44:38 +0800

    find file by modify time

---
+++ PythonCookbook/ch13Utility scripting and system admin/9Finding Files by Name/10find_ByModifytime.py
@@ -0,0 +1,23 @@
+import os
+import time
+
+def modified_within(top, seconds):
+    now = time.time()
+    for path, dirs, files in os.walk(top):
+        for name in files:
+            fullpath = os.path.join(path, name)
+            print('fullpath=', fullpath)
+            if os.path.exists(fullpath):
+                mtime = os.path.getmtime(fullpath)
+                print('mtime=',mtime,'now=',now,'sub=',(now - mtime))
+                if mtime > (now - seconds):
+                    print(fullpath,'\n')
+
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) != 3:
+        print('Usage: {} dir seconds'.format(sys.argv[0]))
+        raise SystemExit(1)
+    
+    modified_within(sys.argv[1], float(sys.argv[2]))
+
--- PythonCookbook/ch13Utility scripting and system admin/9Finding Files by Name/9findFiles_ByName.py
+++ PythonCookbook/ch13Utility scripting and system admin/9Finding Files by Name/9findFiles_ByName.py
@@ -8,5 +8,6 @@ def findfile(start, name):
             full_path = os.path.join(start, relpath, name)
             print(os.path.normpath(os.path.abspath(full_path)))
 
+            
 if __name__ == "__main__":
     findfile(sys.argv[1], sys.argv[2])
