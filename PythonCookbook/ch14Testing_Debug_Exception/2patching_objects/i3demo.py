211178a137fc19f4fc25af53efe8bb4171922506
greatabel <myreceiver2for2github@gmail.com>
2016-01-30 23:24:47 +0800

    mock patch

--- PythonCookbook/ch14Testing_Debug_Exception/2patching_objects/i3demo.py
+++ PythonCookbook/ch14Testing_Debug_Exception/2patching_objects/i3demo.py
@@ -12,11 +12,18 @@ thing.method.assert_called_with(3, 4, 5, key='value')
 # thing.method.assert_called_with(1, 2, 3, 4, 5, key='value')
 
 mock = Mock(side_effect=KeyError('testerror'))
-mock()
+# mock()
 
+import moduletest
 
 @patch('moduletest.ClassName2')
 @patch('moduletest.ClassName1')
 def test(MockClass1, MockClass2):
     moduletest.ClassName1()
     moduletest.ClassName2()
+    assert MockClass1 is moduletest.ClassName1
+    assert MockClass2 is moduletest.ClassName2
+    assert MockClass1.called
+    assert MockClass2.called
+
+test()
