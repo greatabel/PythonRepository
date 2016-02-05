469b30f301e50fb2dacd8bf35d2a0a13fff49bc3
greatabel <myreceiver2for2github@gmail.com>
2016-02-05 16:45:43 +0800

    skip failure

---
+++ PythonCookbook/ch14Testing_Debug_Exception/5skipOranticipating_testFailure/i5skip_testfailure.py
@@ -0,0 +1,15 @@
+import unittest
+import os
+import platform
+
+class Tests(unittest.TestCase):
+    def test_0(self):
+        self.assertTrue(True)
+
+    @unittest.skip('skipped test')
+    def test_1(self):
+        self.fail('should have failed')
+
+
+if __name__ == "__main__":
+    unittest.main()
