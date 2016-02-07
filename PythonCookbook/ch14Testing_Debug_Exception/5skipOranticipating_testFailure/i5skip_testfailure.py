3cf3a31441d9e341f34b1b5210a3a6def647cfec
greatabel <myreceiver2for2github@gmail.com>
2016-02-07 21:54:18 +0800

    skipif

--- PythonCookbook/ch14Testing_Debug_Exception/5skipOranticipating_testFailure/i5skip_testfailure.py
+++ PythonCookbook/ch14Testing_Debug_Exception/5skipOranticipating_testFailure/i5skip_testfailure.py
@@ -4,11 +4,17 @@ import platform
 
 class Tests(unittest.TestCase):
     def test_0(self):
+        print('test_0')
         self.assertTrue(True)
 
     @unittest.skip('skipped test')
     def test_1(self):
         self.fail('should have failed')
+
+    @unittest.skipIf(os.name=='posix', 'Not support on Unix')
+    def test_2(self):
+        print('test_2')
+        import winreg
 
 
 if __name__ == "__main__":
