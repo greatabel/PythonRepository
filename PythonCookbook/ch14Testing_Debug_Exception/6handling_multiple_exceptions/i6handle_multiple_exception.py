be63fbd461e50c5e99f7fc077d3afc85199e75fe
greatabel <myreceiver2for2github@gmail.com>
2016-02-26 23:52:37 +0800

    multiple error handle

---
+++ PythonCookbook/ch14Testing_Debug_Exception/6handling_multiple_exceptions/i6handle_multiple_exception.py
@@ -0,0 +1,9 @@
+import sys
+
+if __name__ == "__main__":
+    try:
+        f = open('test.txt')
+        s = f.readline()
+        i = int(s.strip())
+    except (IOError, ValueError):
+        print("in error block")
