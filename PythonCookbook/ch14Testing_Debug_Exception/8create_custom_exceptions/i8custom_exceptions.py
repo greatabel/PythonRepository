a73fccc70d01a58e983405681f93c32437d290e9
greatabel <myreceiver2for2github@gmail.com>
2016-02-28 22:25:01 +0800

    custom exceptions

---
+++ PythonCookbook/ch14Testing_Debug_Exception/8create_custom_exceptions/i8custom_exceptions.py
@@ -0,0 +1,30 @@
+import socket   #socket模块
+
+
+HOST='10.0.0.245'
+PORT=50007
+s= socket.socket(socket.AF_INET,socket.SOCK_STREAM)   #定义socket类型，网络通信，TCP
+
+
+class NetworkError(Exception):
+    pass
+
+class HostnameError(Exception):
+    pass
+
+class TimeoutError(Exception):
+    pass
+
+class ProtocolError(Exception):
+    pass
+
+if __name__ == "__main__":
+    try:
+        msg = s.recv(1)
+    except TimeoutError as e:
+        print('1', e)
+    except ProtocolError as e:
+        print('2', e)
+    except Exception as e:
+        print('3',e)
+
