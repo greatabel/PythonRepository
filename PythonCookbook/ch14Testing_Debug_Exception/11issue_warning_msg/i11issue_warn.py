1efd2294be478324aafeceb6ea896633dc52f0d9
greatabel <myreceiver2for2github@gmail.com>
2016-03-01 14:51:08 +0800

    warning

---
+++ PythonCookbook/ch14Testing_Debug_Exception/11issue_warning_msg/i11issue_warn.py
@@ -0,0 +1,9 @@
+import warnings
+
+def func(x, y, logfile=None, debug=False):
+    print("#", logfile)
+    if logfile is not None:
+        warnings.warn('logfile argument depressed', DeprecationWarning)
+
+if __name__ == "__main__":
+    func(1, 2,'mylog')
