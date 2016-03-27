4dd0dfbc2921ee2a223aca25e1285a6882b9155a
greatabel <myreceiver2for2github@gmail.com>
2016-03-27 16:50:40 +0800

    i23

--- ProjectEuler/i23Non-abundant sums.py
+++ ProjectEuler/i23Non-abundant sums.py
@@ -4,6 +4,7 @@
 # As 12 is the smallest abundant number, 1 + 2 + 3 + 4 + 6 = 16, the smallest number that can be written as the sum of two abundant numbers is 24. By mathematical analysis, it can be shown that all integers greater than 28123 can be written as the sum of two abundant numbers. However, this upper limit cannot be reduced any further by analysis even though it is known that the greatest number that cannot be expressed as the sum of two abundant numbers is less than this limit.
 
 # Find the sum of all the positive integers which cannot be written as the sum of two abundant numbers.
+import time
 
 def divisorGenerator(n):
     # print("divisorGenerator ",n)
@@ -12,17 +13,39 @@ def divisorGenerator(n):
             # print(i)
             yield i
 
+def is_abundant(number):
+    thesum = sum(list(divisorGenerator(number)))
+    if thesum > number:
+        return True
+    else:
+        return False
+
 def  find_abundant_below_num(num):
-    mydict = {}
+    myset = set()
     for number in range(1,num):
-        thesum = sum(list(divisorGenerator(number)))
-        print("sum=",thesum)
-        if thesum > number:
-            mydict[number] = 1
-        else:
-            mydict[number] = 0
-    # print(mydict)
-    return mydict
+        if is_abundant(number):
+            myset.add(number)
+    print("find_abundant_below_num:",len(myset))
+    return myset
+
+def generate_number_whoIs2abundantSum(abundants,limit):
+    the2sumSet = set()
+    for x in abundants:
+        for y in abundants:
+            if x + y < limit:
+                the2sumSet.add(x+y)
+    print(len(the2sumSet))
+    thesum = 0
+    for i in range(1,limit):
+        thesum += i
+    print('thesum=',thesum,thesum - sum(the2sumSet))
+
+
+
 
 if __name__ == "__main__":
-    find_abundant_below_num(30)
+    tic = time.clock()
+    myset = find_abundant_below_num(28124)
+    toc = time.clock()
+    print(toc - tic)
+    generate_number_whoIs2abundantSum(myset,28124)
