b3d6d53dde9e2097908840450c3f14d65e1e180b
greatabel <myreceiver2for2github@gmail.com>
2016-04-07 16:52:30 +0800

    repeact cycle

--- ProjectEuler/i26Reciprocal cycles.py
+++ ProjectEuler/i26Reciprocal cycles.py
@@ -12,3 +12,40 @@
 # Where 0.1(6) means 0.166666..., and has a 1-digit recurring cycle. It can be seen that 1/7 has a 6-digit recurring cycle.
 
 # Find the value of d < 1000 for which 1/d contains the longest recurring cycle in its decimal fraction part.
+
+
+# http://guozi149.me/foundations/math/516
+from decimal import Decimal
+
+max_index = 0
+maxlen = 0
+
+def count_repeat(numerator,denominator):
+    global max_index
+    global maxlen
+    # numerator *= 10
+    reminder = (numerator  % denominator) * 10
+    print(numerator,'  /  ',denominator,' reminder=', reminder)
+    results = []
+    while reminder not in results:
+        results.append(reminder)
+        reminder = (reminder % denominator) * 10
+
+        # print('reminder=', reminder)
+    if 0 not in results:
+        print('results=', results,'len=',len(results),'denominator=',denominator)
+        if len(results) > maxlen:
+            maxlen = len(results)
+            max_index = denominator
+
+
+def find(n):
+    for i in range(2,n+1):
+        count_repeat(1, i)
+        # print(i,Decimal(1/i))
+    print('###index=',max_index,maxlen)
+
+
+if __name__ == "__main__":
+
+    find(1000)
