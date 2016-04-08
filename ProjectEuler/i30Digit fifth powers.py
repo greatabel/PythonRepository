212f3d93c19dc87a932520ac317306da377a2e21
greatabel <myreceiver2for2github@gmail.com>
2016-04-08 16:39:43 +0800

    i30 solve

--- ProjectEuler/i30Digit fifth powers.py
+++ ProjectEuler/i30Digit fifth powers.py
@@ -12,9 +12,39 @@
 import time
 import math
 
-def find_allpowers(pw):
-    for i in range(1,10):
-        print(i,math.pow(i,pw))
+# 9   999,999,999 531,441 they need to have the same # of digits
+# 8   99,999,999  472,392 nope, still too many
+# 7   9,999,999   413,343 not quite there
+# 6   999,999 354,294 a valid search limit
+
+def circulate_digit_sum(n,pw):
+
+    # print('n=',n)
+    dsum = 0
+    while n  > 0:
+        reminder = n % 10
+        n //= 10
+        # print(reminder,n)
+        dsum += powers[reminder]
+    # print(dsum)
+    return dsum
+
+powers = {}
+
+def find_allpowers(presume,pw):
+    global powers
+    for a in range(10):
+         powers[a] = a ** pw
+    print('powers=',powers)
+
+    mysum = 0
+    for i in range(2, presume):
+        if i == circulate_digit_sum(i,pw):
+            print(i)
+            mysum += i
+    print('mysum=',mysum)
+
+
     
 
 if __name__ == "__main__":
@@ -22,7 +52,8 @@ if __name__ == "__main__":
     # for i in range(0,20):
     #     print(i,fib(i),len(str(fib(i))))
     # find_allpowers(5,5)
-    find_allpowers(4)
+    # find_allpowers(10000,4)
+    find_allpowers(1000000,5)
 
     toc = time.clock()
     print("time=",toc - tic)
