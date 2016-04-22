0242fd552690324e19d5cc8be4fefed445904e3c
greatabel <myreceiver2for2github@gmail.com>
2016-04-22 11:40:17 +0800

    i47Distinct primes factors.py

---
+++ ProjectEuler/i47Distinct primes factors.py
@@ -0,0 +1,29 @@
+# Distinct primes factors
+# Problem 47
+# The first two consecutive numbers to have two distinct prime factors are:
+
+# 14 = 2 × 7
+# 15 = 3 × 5
+
+# The first three consecutive numbers to have three distinct prime factors are:
+
+# 644 = 2² × 7 × 23
+# 645 = 3 × 5 × 43
+# 646 = 2 × 17 × 19.
+
+# Find the first four consecutive integers to have four distinct prime factors. What is the first of these numbers?
+
+import time
+from termcolor import colored
+
+
+def main_process():
+    print(colored('mycount=', 'red'), 'results')
+
+if __name__ == "__main__":
+    tic = time.clock()
+    
+    main_process()
+
+    toc = time.clock()
+    print("time=",toc - tic)
