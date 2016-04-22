ebbaad5fda5b408a8cffe649650e3d675d2095d0
greatabel <myreceiver2for2github@gmail.com>
2016-04-22 11:35:49 +0800

    i46Goldbach's other conjecture.py

--- ProjectEuler/i46Goldbach's other conjecture.py
+++ ProjectEuler/i46Goldbach's other conjecture.py
@@ -15,11 +15,71 @@
 
 
 import time
+import math
 from termcolor import colored
+
+def isPrime(num):
+    # print('num=',num)
+    flag = True
+    if num == 1:
+        flag = False
+    for i in range(2, round(math.sqrt(num))+1):
+        if num % i == 0:
+            # print(num ,'%',i )
+            flag = False
+    return flag
+
+def find_prime(bound):
+    primeUnderSqrtN = []
+    for i in range(2,round(math.sqrt(bound))+1):
+        if isPrime(i):
+            primeUnderSqrtN.append(i)
+    # print("primeUnderSqrtN=",primeUnderSqrtN)
+    mylist = list(range(1,bound))
+    
+    i = 0
+    count = 0
+    while i!= len(primeUnderSqrtN):
+        for j in range(i+1,len(mylist)):
+            if mylist[j] % primeUnderSqrtN[i] == 0:
+                mylist[j] = 0
+                count += 1
+        mylist.sort()
+        mylist = mylist[count:]
+        count = 0
+        i += 1
+        # print("i=",i)
+    mylist.remove(1)
+    mylist.append(2)
+    mylist.sort()
+    return mylist
+
+
+
+def checkconjecture(n,primes):
+    flag = False
+    for p in primes:
+        if n > p:
+            r = math.sqrt( (n - p) / 2 )
+            if int(r) == r:
+                print(n,"=",p,"+2* ",r,"^2")
+                flag = True
+    return flag
+        
 
 
 def main_process():
+    bound = 10000
+    primes = find_prime(bound)
+    for i in range(1,bound):
+        if i % 2 == 1 and i > 1 and i not in primes:
+            if not checkconjecture(i,primes):
+                print(i,checkconjecture(i,primes))
+                break
+
     print(colored('mycount=', 'red'), 'results')
+
+
 
 if __name__ == "__main__":
     tic = time.clock()
