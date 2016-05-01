a9528e22953d3de9d741988fe71c92076033d34c
greatabel <myreceiver2for2github@gmail.com>
2016-05-01 10:10:33 +0800

    i52Permuted multiples.py

--- ProjectEuler/i52Permuted multiples.py
+++ ProjectEuler/i52Permuted multiples.py
@@ -12,8 +12,42 @@
 import time
 from termcolor import colored
 
+def get_digital(number):
+    results = []
+    if number == 0:
+        results = [0]
+    while number > 0:
+        result = number % 10
+        number = number // 10
+        results.append(result)
+    # print('results=', results)
+    return results
+
+def condition(number):
+    L  = []
+    for i in range(1,7):
+        tmp = get_digital(number * i)
+        tmp.sort()
+        L.append(tmp)
+        print('tmp=',tmp)
+    if all(x == L[0] for x in L):
+        print(L)
+        return True
+
+
+
 
 def main_process():
+    flag = True
+    i = 1
+    while flag:
+        if condition(i):
+            flag = False
+        else:
+            flag = True
+        i += 1
+        print('i=',i)
+
     print(colored('mycount=', 'red'), 'results')
 
 if __name__ == "__main__":
