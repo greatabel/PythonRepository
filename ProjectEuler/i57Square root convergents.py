55f2cc12f4bd582bfa5a4ffbe372cbbfac20ca4a
greatabel <myreceiver2for2github@gmail.com>
2016-05-05 10:24:44 +0800

    init i57

---
+++ ProjectEuler/i57Square root convergents.py
@@ -0,0 +1,32 @@
+# Square root convergents
+# Problem 57
+# It is possible to show that the square root of two can be expressed as an infinite continued fraction.
+
+# √ 2 = 1 + 1/(2 + 1/(2 + 1/(2 + ... ))) = 1.414213...
+
+# By expanding this for the first four iterations, we get:
+
+# 1 + 1/2 = 3/2 = 1.5
+# 1 + 1/(2 + 1/2) = 7/5 = 1.4
+# 1 + 1/(2 + 1/(2 + 1/2)) = 17/12 = 1.41666...
+# 1 + 1/(2 + 1/(2 + 1/(2 + 1/2))) = 41/29 = 1.41379...
+
+# The next three expansions are 99/70, 239/169, and 577/408, but the eighth expansion, 1393/985, 
+# is the first example where the number of digits in the numerator exceeds the number of digits in the denominator.
+
+# In the first one-thousand expansions, how many fractions contain a numerator with more digits than denominator?
+
+import time
+from termcolor import colored
+
+
+def main_process():
+    print(colored('mycount=', 'red'), 'results')
+
+if __name__ == "__main__":
+    tic = time.clock()
+    
+    main_process()
+
+    toc = time.clock()
+    print("time=",toc - tic)
