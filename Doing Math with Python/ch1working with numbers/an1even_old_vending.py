4e9b139227b5069e9b9f43caa13241c3bf0257bc
greatabel <myreceiver2for2github@gmail.com>
2016-10-29 23:20:01 +0800

    answer2enhanced_multiple_table.py

---
+++ Doing Math with Python/ch1working with numbers/an1even_old_vending.py
@@ -0,0 +1,22 @@
+def even_odd_vending(num):
+
+    if (num % 2) == 0:
+        print('Even')
+    else:
+        print('Odd')
+    count = 1
+    while count <= 9:
+        num += 2
+        print(num)
+        # increment the count of numbers printed
+        count += 1   
+
+if __name__ == '__main__':
+    try:
+        num = float(input('Enter an integer: '))
+        if num.is_integer():
+            even_odd_vending(int(num))
+        else:
+            print('Please enter an integer')            
+    except ValueError:
+        print('Please enter a number')
--- Doing Math with Python/ch1working with numbers/i2generating multiplication tables.py
+++ Doing Math with Python/ch1working with numbers/i2generating multiplication tables.py
@@ -1,15 +1,12 @@
-def multi_table(a, n):
-    for i in range(1, n+1):
+def multi_table(a):
+    for i in range(1, 11):
         print('{0} x {1} = {2}'.format(a, i, a*i))
 
 def main():
     try:
         a = float(input('Enter a number: '))
-        n = float(input('Enter the number of multiples: '))
-        if not n.is_integer() or n < 0:
-            print('The number of multiples should be a positive integer')
-        else:
-            multi_table(a, int(n))
+        
+        multi_table(float(a))
     except ValueError:
         print('You entered an invalid input')    
 
