9ad2fb80e469df5687031f7ee69cab72ef9326ab
greatabel <myreceiver2for2github@gmail.com>
2016-10-30 20:55:07 +0800

    change in i1list

--- Doing Math with Python/ch2visualiztion data with graphs/i1list.py
+++ Doing Math with Python/ch2visualiztion data with graphs/i1list.py
@@ -1,2 +1,7 @@
 simplelist = [1, 2, 3]
 print(simplelist)
+simplelist = ['a string','b string','c string']
+print(simplelist)
+simplelist.append(1)
+print(simplelist)
+print('type:',type(simplelist))
