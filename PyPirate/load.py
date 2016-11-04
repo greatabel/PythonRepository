2b56ef249b8ebc389506c8c7410c9c553b73dbf1
greatabel <myreceiver2for2github@gmail.com>
2016-11-04 17:56:52 +0800

    screenshot

--- PyPirate/load.py
+++ PyPirate/load.py
@@ -15,6 +15,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
+from selenium.webdriver.common.action_chains import ActionChains
 
 with open ("source.html", "r") as myfile:
     src=myfile.read()
@@ -32,24 +33,32 @@ print('url count:', len(t))
 print('in load')
 # step 2
 # driver = webdriver.Chrome() # if you want to use chrome, replace Firefox() with Chrome()
-# driver = webdriver.Firefox()
+driver = webdriver.Firefox()
 # driver = webdriver.PhantomJS(service_args=['--ignore-ssl-errors=true'])
 pre_url = "https://www.packtpub.com/mapt/book/Web Development/9781784393656/"
 test_url  = pre_url + "1"
-# driver.get(test_url) # load the web page
+driver.get(test_url) # load the web page
 
 
 # WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.TAG_NAME, "h2")))
 
-# try:
-#     element = WebDriverWait(driver, 20).until(
-#         EC.presence_of_element_located((By.ID, "reader-content"))
-#     )
-#     # waits till the element with the specific id appears
-#     src = driver.page_source # gets the html source of the page
-#     print('#'*20,len(src),src[0:100])
-# finally:
-#     driver.quit()
+try:
+    element = WebDriverWait(driver, 15).until(
+        EC.presence_of_element_located((By.ID, "reader-content"))
+    )
+    # waits till the element with the specific id appears
+    src = driver.page_source # gets the html source of the page
+    print('#'*20,len(src),src[0:150],'*'*10)
+    Html_file= open("filename.html","w")
+    Html_file.write(src)
+    Html_file.close()
+    driver.save_screenshot('screenie.png')
+    # save_me = ActionChains(driver).key_down(Keys.CONTROL)\
+    #      .key_down('s').key_up(Keys.CONTROL).key_up('s')
+    # save_me.perform()
+
+finally:
+    driver.quit()
 
 
 
