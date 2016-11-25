89ee0f75ac71d9693f5222d730aab89fe3e0b078
greatabel <myreceiver2for2github@gmail.com>
2016-11-25 18:14:17 +0800

    begin euler

--- ProjectEuler/i68Magic 5-gon ring.py
+++ ProjectEuler/i68Magic 5-gon ring.py
@@ -20,8 +20,6 @@
 # Using the numbers 1 to 10, and depending on arrangements, it is possible to form 16- and 17-digit strings. What is the maximum 16-digit string 
 # for a "magic" 5-gon ring?
 
-
-
 import time
 from termcolor import colored
 
