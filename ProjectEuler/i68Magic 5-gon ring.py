37c2a13e4ac994196e0d6a9af448b4ffd8026c74
greatabel <myreceiver2for2github@gmail.com>
2016-12-06 17:48:04 +0800

    i68 show 6 situation

--- ProjectEuler/i68Magic 5-gon ring.py
+++ ProjectEuler/i68Magic 5-gon ring.py
@@ -17,20 +17,41 @@
 # 12  1,6,5; 3,5,4; 2,4,6
 # By concatenating each group it is possible to form 9-digit strings; the maximum string for a 3-gon ring is 432621513.
 
-# Using the numbers 1 to 10, and depending on arrangements, it is possible to form 16- and 17-digit strings. What is the maximum 16-digit string 
+# Using the numbers 1 to 10, and depending on arrangements, it is possible to form 16- and 17-digit strings. 
+# What is the maximum 16-digit string 
 # for a "magic" 5-gon ring?
 
 import time
 from termcolor import colored
 
+import itertools
 
-def main_process():
+def main_process(n):
+    
+    all_possilble = list(itertools.permutations(range(1,n+1), n))
+    matches = []
+    for l in all_possilble:
+        test = is_gong(l)
+        if test is not None:
+            matches.append(test)
+    print(matches)
     print(colored('mycount=', 'red'), 'results')
+
+def is_gong(l):
+    sum_I = l[0]+l[1]+l[2]
+    sum_II = l[2]+l[3]+l[4]
+    sum_III = l[1]+l[4]+l[5]
+    if sum_I == sum_II and sum_II == sum_III:
+        print(l[0],l[1],l[2],'#',l[3],l[2],l[4],'#',l[5],l[4],l[1])
+        return int(str(l[0])+str(l[1])+str(l[2])+str(l[3])+\
+                str(l[2])+str(l[4])+str(l[5])+str(l[4])+str(l[1]))
+    else:
+        return None
 
 if __name__ == "__main__":
     tic = time.clock()
     
-    main_process()
+    main_process(6)
 
     toc = time.clock()
     print("time=",toc - tic)
