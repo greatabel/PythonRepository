43089925f17d8e92b15a40c0af9cd1ccc6c93af4
greatabel <myreceiver2for2github@gmail.com>
2016-12-08 23:07:26 +0800

    i9customize_axes.py

---
+++ Doing Math with Python/ch2visualiztion data with graphs/i9customize_axes.py
@@ -0,0 +1,7 @@
+from pylab import axis, plot, show
+
+nyc_temp = [53.9, 56.3, 56.4, 53.4, 54.5, 55.8, 56.8, 55.0, 55.3, 54.0, 56.7, 56.4, 57.3]
+plot(nyc_temp, marker='o')
+print(axis())
+print(axis(ymin=0))
+show()
