1cfb3c5a99eca81e470ca6b419492a4fb345b255
greatabel <myreceiver2for2github@gmail.com>
2016-12-09 10:51:12 +0800

    i12plotting_with_formulas.py

---
+++ Doing Math with Python/ch2visualiztion data with graphs/i12plotting_with_formulas.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+
+def draw_graph(x, y):
+    plt.plot(x, y, marker='o')
+    plt.xlabel('Distance in meters')
+    plt.ylabel('G force in newtons')
+    plt.title(
+        'G force and distance'
+        )
+    plt.show()
+
+def generate_F_r():
+       # Generate values for r
+    r = range(100, 1001, 50)
+    # Empty list to store the calculated values of F 
+    F = []
+    # Constant, G
+    G = 6.674*(10**-11)
+   # Two masses
+    m1 = 0.5
+    m2 = 1.5
+    for dist in r:
+           force = G*(m1*m2)/(dist**2)
+           F.append(force)
+    draw_graph(r, F)
+
+if __name__ == '__main__':
+    generate_F_r()
