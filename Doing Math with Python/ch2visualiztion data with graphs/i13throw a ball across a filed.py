e342d96d6ac65e93b3023ef89c5d5b92b767cc7d
greatabel <myreceiver2for2github@gmail.com>
2017-01-11 23:03:17 +0800

    add draw_trajectory

--- Doing Math with Python/ch2visualiztion data with graphs/i13throw a ball across a filed.py
+++ Doing Math with Python/ch2visualiztion data with graphs/i13throw a ball across a filed.py
@@ -16,6 +16,15 @@ def draw_graph(x, y):
     plt.title('Projectile motion at different initial velocities(速度) and angles(角度)')
     # plt.show()
 
+def draw_trajectory(u, theta, t_flight):
+    x = []
+    y = []
+    intervals = frange(0, t_flight, 0.001)
+    for t in intervals:
+        x.append(u*math.cos(theta)*t)
+        y.append(u*math.sin(theta)*t - 0.5*g*t*t)
+
+    draw_graph(x, y)
 
 if __name__ == "__main__":
     # draw_graph(10, 10)
