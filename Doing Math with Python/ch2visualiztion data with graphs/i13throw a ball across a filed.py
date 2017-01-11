335b23d0178daffb2b8044e6ce99a56ff876a107
greatabel <myreceiver2for2github@gmail.com>
2017-01-11 22:57:57 +0800

    import math

--- Doing Math with Python/ch2visualiztion data with graphs/i13throw a ball across a filed.py
+++ Doing Math with Python/ch2visualiztion data with graphs/i13throw a ball across a filed.py
@@ -1,5 +1,10 @@
 import matplotlib.pyplot as plt
+import math
+
 
 def frange(start, final, increment):
     numbers = []
+    while start < final:
+        numbers.append(start)
+        start = start + increment
     return numbers
