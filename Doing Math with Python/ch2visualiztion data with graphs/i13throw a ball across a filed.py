29c2a9178e13f2b9cc163458c4e50668b26b5cd4
greatabel <myreceiver2for2github@gmail.com>
2017-01-11 23:01:00 +0800

    add draw_graph

--- Doing Math with Python/ch2visualiztion data with graphs/i13throw a ball across a filed.py
+++ Doing Math with Python/ch2visualiztion data with graphs/i13throw a ball across a filed.py
@@ -8,3 +8,14 @@ def frange(start, final, increment):
         numbers.append(start)
         start = start + increment
     return numbers
+
+def draw_graph(x, y):
+    plt.plot(x, y)
+    plt.xlabel('x-coordinate')
+    plt.ylabel('y-coordinate')
+    plt.title('Projectile motion at different initial velocities(速度) and angles(角度)')
+    # plt.show()
+
+
+if __name__ == "__main__":
+    # draw_graph(10, 10)
