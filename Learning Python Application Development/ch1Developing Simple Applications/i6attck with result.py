dca56ef85782f55d933287ca52247b97ff8941ae
greatabel <myreceiver2for2github@gmail.com>
2017-02-06 23:50:40 +0800

    reformat

--- Learning Python Application Development/ch1Developing Simple Applications/i6attck with result.py
+++ Learning Python Application Development/ch1Developing Simple Applications/i6attck with result.py
@@ -13,13 +13,14 @@ def main():
     print("\033[1m" + "Attack of The Orcs v0.0.1;" + "\033[0m")
 
     msg = (
-    "The war between humans and their arch enemies, Orcs, was in the "
-    "offing. Sir Foo, one of the brave knights guarding the southern "
-    "plains began a long journey towards the east through an unknown "
-    "dense forest. On his way, he spotted a small isolated settlement."
-    " Tired and hoping to replenish his food stock, he decided to take"
-    " a detour. As he approached the village, he saw five huts. There "
-    "was no one to be seen around. Hesitantly, he  decided to enter..")
+            "The war between humans and their arch enemies, Orcs, was in the "
+            "offing. Sir Foo, one of the brave knights guarding the southern "
+            "plains began a long journey towards the east through an unknown "
+            "dense forest. On his way, he spotted a small isolated settlement."
+            " Tired and hoping to replenish his food stock, he decided to take"
+            " a detour. As he approached the village, he saw five huts. There "
+            "was no one to be seen around. Hesitantly, he  decided to enter.."
+          )
 
     print(textwrap.fill(msg, width=width))
 
@@ -39,13 +40,13 @@ def main():
         msg = "\033[1m" + "Choose a hut number to enter (1-5): " + "\033[0m"
         user_choice = input("\n" + msg)
         idx = int(user_choice)
-        
+
         # Print the occupant info
         print("Revealing the occupants...")
         # print('*'*5, idx, huts[idx-1])
         msg = ""
         for i in range(len(huts)):
-            occupant_info = "<%d:%s>"%(i+1, huts[i])
+            occupant_info = "<%d:%s>" % (i + 1, huts[i])
             if i + 1 == idx:
                 occupant_info = "\033[1m" + occupant_info + "\033[0m"
             msg += occupant_info + " "
@@ -60,7 +61,6 @@ def main():
         else:
             print("\033[1m" + "Congratulations! YOU WIN!!!" + "\033[0m")
 
-        
         print(dotted_line)
         keep_playing = input("Play again? Yes(y)/No(n):")
 
