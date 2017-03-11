c75ba06c35f3a2e0e361929723bf0e67830405c4
greatabel <myreceiver2for2github@gmail.com>
2017-03-11 17:10:39 +0800

    handle

--- PyDouList/douban_list.py
+++ PyDouList/douban_list.py
@@ -1,6 +1,9 @@
 
 from urllib.request import Request, urlopen
 from urllib.error import URLError, HTTPError
+from pathlib import Path
+import datetime
+
 
 doulist_page = 'https://www.douban.com/people/greatabel/doulists/all'
 
@@ -9,7 +12,7 @@ def get_html(url):
         request = Request(url, None, {"User-Agent": "Mozilla/5.0 AppleWebKit/537.36"})
         response = urlopen(request)
         data = response.read().decode('utf-8')
-        print(data)
+        print('len(data)=',len(data))
     except HTTPError as e:
         # do something
         print('Error code: ', e.code)
@@ -18,8 +21,17 @@ def get_html(url):
         print('Reason: ', e.reason)
     return data
 
+def save_to_localfile(filename, content):
+    with open(filename, 'wt') as f:
+        f.write(content)
+
 def main():
-    get_html(doulist_page)
+    filename01 = '@@@01my_all_doulist#'+datetime.datetime.today().strftime('%Y-%m-%d')
+    file01 = Path("./" + filename01)
+    if not file01.is_file():
+        content = get_html(doulist_page)
+        save_to_localfile(filename01, content)
+
 
 if __name__ == "__main__":
     main()
