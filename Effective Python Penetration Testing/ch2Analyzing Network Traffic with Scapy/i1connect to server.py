a687f1ebd65ce74c0a2462bdbd957a50abb8e3be
greatabel <myreceiver2for2github@gmail.com>
2017-07-11 11:00:50 +0800

    i1connect to server.py

---
+++ Effective Python Penetration Testing/ch2Analyzing Network Traffic with Scapy/i1connect to server.py
@@ -0,0 +1,25 @@
+import socket #Imported sockets module
+import sys
+TCP_IP = '127.0.0.1'
+TCP_PORT = 8090 #Reserve a port
+BUFFER_SIZE = 1024
+MESSAGE_TO_SERVER = "Hello, World!"
+
+try:
+   #Create an AF_INET (IPv4), STREAM socket (TCP)
+   tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+except socket.error as e:
+   print('Error occurred while creating socket. Error code: ' + str(e[0])
+        + ' , Error message : ' + e[1] )
+   sys.exit()
+
+tcp_socket.connect((TCP_IP, TCP_PORT))
+try :
+   #Sending message
+   tcp_socket.send(MESSAGE_TO_SERVER)
+except socket.error as e:
+   print('Error occurred while sending data to server. Error code: ' +
+  str(e[0]) + ' , Error message : ' + e[1])
+   sys.exit()
+
+print('Message to the server send successfully')
