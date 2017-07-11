c017b1a84883bb30d6c3ec0708d4a85b601fc0f0
greatabel <myreceiver2for2github@gmail.com>
2017-07-11 11:17:35 +0800

    i2 sendall

--- Effective Python Penetration Testing/ch2Analyzing Network Traffic with Scapy/i2receive data.py
+++ Effective Python Penetration Testing/ch2Analyzing Network Traffic with Scapy/i2receive data.py
@@ -3,9 +3,15 @@ TCP_IP = '127.0.0.1'
 TCP_PORT = 8090
 BUFFER_SIZE = 1024 
 
+def sendall():
+    data = connection.recv(BUFFER_SIZE)
+    print("Message from client:", data)
+
+    connection.sendall("Thanks for connecting")
+    connection.close()
+
 #Normally use 1024, to get fast response from the server
 # use small size
-
 try:
       #Create an AF_INET (IPv4), STREAM socket (TCP)
       tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -18,6 +24,14 @@ tcp_socket.bind((TCP_IP, TCP_PORT))
 # Listen for incoming connections  (max queued connections: 2)
 tcp_socket.listen(2)
 print('Listening..')
-#Waits for incoming connection (blocking call)
-connection, address = tcp_socket.accept()
-print('Connected with:', address)
+
+while True:
+    #Waits for incoming connection (blocking call)
+    connection, address = tcp_socket.accept()
+    print('Connected with:', address)
+
+    data = connection.recv(BUFFER_SIZE)
+    print('Message from client ->', data)
+
+    connection.sendall(b'Thanks for connecting')
+
