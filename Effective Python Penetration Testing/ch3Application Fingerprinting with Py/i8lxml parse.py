0a178443f37c1ec3190508afc16309f1b3605a99
greatabel <myreceiver2for2github@gmail.com>
2017-08-05 23:45:35 +0800

    i8lxml parse.py

---
+++ Effective Python Penetration Testing/ch3Application Fingerprinting with Py/i8lxml parse.py
@@ -0,0 +1,12 @@
+from lxml import html
+import requests
+import itertools
+
+
+response = requests.get('http://packtpub.com/')
+tree = html.fromstring(response.content)
+#Create the list of Books:
+books = tree.xpath('//div[@class="book-block-title"]/text()')
+
+
+print(books)
