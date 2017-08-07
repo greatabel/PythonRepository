b52e6ee292a30a3432ac40ca1fc61806113c0dc4
greatabel <myreceiver2for2github@gmail.com>
2017-08-07 23:49:13 +0800

    i10OS fingerprinting.py

---
+++ Effective Python Penetration Testing/ch3Application Fingerprinting with Py/i10OS fingerprinting.py
@@ -0,0 +1,15 @@
+import logging
+logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
+
+from scapy.all import *
+# To sniff scapy requires to run as sudo.
+# sudo python3 i10OS\ fingerprinting.py 
+
+
+ans,unans=srloop(IP(dst="192.168.0.1")/TCP(dport=80,flags="S"))
+temp = 0
+for s,r in ans:
+    temp = r[TCP].seq - temp
+    print( str(r[TCP].seq) + "\t+" + str(temp) )
+
+
