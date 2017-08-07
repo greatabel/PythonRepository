2ef762247680d87049854aa6a68e3730f2889055
greatabel <myreceiver2for2github@gmail.com>
2017-08-07 17:30:56 +0800

    i9email scraper.py

--- Effective Python Penetration Testing/ch3Application Fingerprinting with Py/i9email scraper.py
+++ Effective Python Penetration Testing/ch3Application Fingerprinting with Py/i9email scraper.py
@@ -1,7 +1,10 @@
 from bs4 import BeautifulSoup
 import requests
 import requests.exceptions
-import urlparse 
+# import urlparse 
+import urllib.parse
 from collections import deque
 import re
 
+urls = deque(['https://www.packtpub.com/'])
+print(type(urls), urls)
