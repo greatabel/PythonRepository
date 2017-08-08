ef1f1806748f788eb2acb4df76288f6e5d7f111e
greatabel <myreceiver2for2github@gmail.com>
2017-08-08 14:22:15 +0800

    i5CSRF.py

---
+++ Effective Python Penetration Testing/ch4Attack Scripting with Python/i5CSRF.py
@@ -0,0 +1,17 @@
+import mechanize
+
+cookies = mechanize.CookieJar()
+
+cookie_opener = mechanize.build_opener(mechanize.HTTPCookieProcessor(cookies))
+
+mechanize.install_opener(cookie_opener) 
+
+url = "http://www.webscantest.com/crosstraining/aboutyou.php"
+
+
+res = mechanize.urlopen(url)
+content = res.read()
+print len(content), content[0:100]
+    
+  
+    
