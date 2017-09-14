4d64d24ea1ec274581062919a1c5821c32b373c5
greatabel <myreceiver2for2github@gmail.com>
2017-09-14 12:08:54 +0800

    i1complex_number.py

---
+++ Coding the Matrix/i1complex_number.py
@@ -0,0 +1,8 @@
+# http://resources.codingthematrix.com/
+from plotting  import plot
+
+S = {2 + 2j,3 + 2j,1.75 + 1j,2 + 1j,2.25 + 1j,2.5+1j,2.75+1j,3+1j,3.25+1j}
+plot(S, 4)
+
+variable = input('input something!: ')
+print('variable:',variable)
