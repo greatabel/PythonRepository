1d6138f6a67a25d947d45625fdaf60f3359f12b7
greatabel <myreceiver2for2github@gmail.com>
2017-09-30 18:32:39 +0800

    unitest

--- Test-Driven Development with Python/2Extending Our Functional Test Using the unittest Module/functional_test.py
+++ Test-Driven Development with Python/2Extending Our Functional Test Using the unittest Module/functional_test.py
@@ -1,8 +1,23 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
 
-assert 'To-Do' in browser.title
+class NewVisitorTest(unittest.TestCase):
 
-browser.quit()
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+
+        self.browser.get('http://localhost:8000')
+
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+        # She is invited to enter
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
