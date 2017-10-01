0b3085738b73922bd40afb74f13cbf0f4eee818b
greatabel <myreceiver2for2github@gmail.com>
2017-10-01 17:35:26 +0800

    First unit test and url mapping, dummy view

--- Test-Driven Development with Python/3Testing a Simple Home Page with Unit Tests/superlists/lists/tests.py
+++ Test-Driven Development with Python/3Testing a Simple Home Page with Unit Tests/superlists/lists/tests.py
@@ -1,7 +1,16 @@
+from django.urls import resolve
 from django.test import TestCase
+from lists.views import home_page
+
 
 # Create your tests here.
-class SmokeTest(TestCase):
+# class SmokeTest(TestCase):
 
-    def test_bad_maths(self):
-        self.assertEqual(1 + 1, 3)
+#     def test_bad_maths(self):
+#         self.assertEqual(1 + 1, 3)
+class HomePageTest(TestCase):
+
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        # print('#'*10,'found=', found)
+        self.assertEqual(found.func, home_page)
