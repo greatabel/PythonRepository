b2cdf57aba987c0dea34b10e11e45633aff67ef3
greatabel <myreceiver2for2github@gmail.com>
2017-10-02 21:26:51 +0800

    What Are We Doing with All These Tests? (And, Refactoring)

---
+++ Test-Driven Development with Python/4What Are We Doing with All These Tests? (And, Refactoring)/functional_test.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+
+import unittest
+
+
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+
+        self.browser.get('http://localhost:8000')
+
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+        # She is invited to enter
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
