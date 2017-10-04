62060e5782c03779e0f41655d1761f8eb9f1b7f9
greatabel <myreceiver2for2github@gmail.com>
2017-10-04 20:46:10 +0800

    placeholder

--- Test-Driven Development with Python/4What Are We Doing with All These Tests? (And, Refactoring)/functional_test.py
+++ Test-Driven Development with Python/4What Are We Doing with All These Tests? (And, Refactoring)/functional_test.py
@@ -1,5 +1,5 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import keys
+from selenium.webdriver.common.keys import Keys
 import time
 import unittest
 
@@ -20,6 +20,12 @@ class NewVisitorTest(unittest.TestCase):
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
         
+        # She is invited to enter a to-do item straight away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
         self.fail('Finish the test!')
 
         # She is invited to enter
