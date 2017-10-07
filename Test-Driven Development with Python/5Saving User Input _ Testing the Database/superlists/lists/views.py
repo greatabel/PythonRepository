1ad559dc8e0b705fea7968d5748042e1f74007c9
greatabel <myreceiver2for2github@gmail.com>
2017-10-07 22:41:21 +0800

    pass post template

--- Test-Driven Development with Python/5Saving User Input _ Testing the Database/superlists/lists/views.py
+++ Test-Driven Development with Python/5Saving User Input _ Testing the Database/superlists/lists/views.py
@@ -5,6 +5,6 @@ from django.http import HttpResponse
 # Create your views here.
 def home_page(request):
     # return HttpResponse('<html><title>To-Do lists</title></html>')
-    if request.method == 'POST':
-        return HttpResponse(request.POST['item_text'])
-    return render(request, 'home.html')
+    return render(request, 'home.html', {
+                'new_item_text': request.POST.get('item_text', ''),
+            })
