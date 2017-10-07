8dd906e9da34ad7eb7761122a4ae977c35b5d2a0
greatabel <myreceiver2for2github@gmail.com>
2017-10-07 21:45:23 +0800

    test template

--- Test-Driven Development with Python/4What Are We Doing with All These Tests? (And, Refactoring)/superlists/lists/tests.py
+++ Test-Driven Development with Python/4What Are We Doing with All These Tests? (And, Refactoring)/superlists/lists/tests.py
@@ -1,7 +1,7 @@
 from django.urls import resolve
 from django.test import TestCase
 from django.http import HttpRequest
-
+from django.template.loader import render_to_string
 from lists.views import home_page
 
 
@@ -21,6 +21,8 @@ class HomePageTest(TestCase):
         request = HttpRequest()
         response = home_page(request)
         html = response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+        expected_html = render_to_string('home.html')
+        self.assertEqual(html, expected_html)
+        # self.assertTrue(html.startswith('<html>'))
+        # self.assertIn('<title>To-Do lists</title>', html)
+        # self.assertTrue(html.strip().endswith('</html>'))
