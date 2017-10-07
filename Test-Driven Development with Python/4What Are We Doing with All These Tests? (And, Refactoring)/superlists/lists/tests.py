3bf8c83120fce2d937a17ffe1d874629024ec30c
greatabel <myreceiver2for2github@gmail.com>
2017-10-07 21:47:32 +0800

    client.get & assertTemplateUsed

--- Test-Driven Development with Python/4What Are We Doing with All These Tests? (And, Refactoring)/superlists/lists/tests.py
+++ Test-Driven Development with Python/4What Are We Doing with All These Tests? (And, Refactoring)/superlists/lists/tests.py
@@ -18,11 +18,13 @@ class HomePageTest(TestCase):
         self.assertEqual(found.func, home_page)
 
     def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
+        response = self.client.get('/')
         html = response.content.decode('utf8')
-        expected_html = render_to_string('home.html')
-        self.assertEqual(html, expected_html)
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>To-Do lists</title>', html)
+        self.assertTrue(html.strip().endswith('</html>'))
+
+        self.assertTemplateUsed(response, 'home.html')
         # self.assertTrue(html.startswith('<html>'))
         # self.assertIn('<title>To-Do lists</title>', html)
         # self.assertTrue(html.strip().endswith('</html>'))
