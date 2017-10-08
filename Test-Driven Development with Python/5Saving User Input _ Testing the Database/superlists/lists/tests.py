0b121095a67a41675ad077d321876a326f5f3ef5
greatabel <myreceiver2for2github@gmail.com>
2017-10-08 23:40:18 +0800

    test count in get

--- Test-Driven Development with Python/5Saving User Input _ Testing the Database/superlists/lists/tests.py
+++ Test-Driven Development with Python/5Saving User Input _ Testing the Database/superlists/lists/tests.py
@@ -11,6 +11,10 @@ from lists.models import Item
 #     def test_bad_maths(self):
 #         self.assertEqual(1 + 1, 3)
 class HomePageTest(TestCase):
+    
+    def test_only_saves_items_when_necessary(self): 
+        self.client.get('/')
+        self.assertEqual(Item.objects.count(), 0)
 
     def test_uses_home_template(self):
         response = self.client.get('/')
