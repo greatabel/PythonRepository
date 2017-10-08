bba1d04e9d72de8a64ca3d9d4ba5efc375f94146
greatabel <myreceiver2for2github@gmail.com>
2017-10-08 23:53:58 +0800

    redirect refactor

--- Test-Driven Development with Python/5Saving User Input _ Testing the Database/superlists/lists/tests.py
+++ Test-Driven Development with Python/5Saving User Input _ Testing the Database/superlists/lists/tests.py
@@ -28,9 +28,11 @@ class HomePageTest(TestCase):
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, 'A new list item')
 
+    def test_redirects_after_POST(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})       
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response['location'], '/')
-
+        
 
 class ItemModelTest(TestCase):
 
