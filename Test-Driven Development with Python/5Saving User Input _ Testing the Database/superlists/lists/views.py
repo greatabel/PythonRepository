5d12df721b05602696c52341acac3dda9aed419c
greatabel <myreceiver2for2github@gmail.com>
2017-10-08 23:41:56 +0800

    refactor post&get

--- Test-Driven Development with Python/5Saving User Input _ Testing the Database/superlists/lists/views.py
+++ Test-Driven Development with Python/5Saving User Input _ Testing the Database/superlists/lists/views.py
@@ -5,10 +5,15 @@ from lists.models import Item
 
 # Create your views here.
 def home_page(request):
-    item = Item()
-    item.text = request.POST.get('item_text', '')
-    item.save()
+    if request.method == 'POST':
+        new_item_text = request.POST['item_text']
+        Item.objects.create(text=new_item_text)
+    else:
+        new_item_text = ''
+    # item = Item()
+    # item.text = request.POST.get('item_text', '')
+    # item.save()
     # return HttpResponse('<html><title>To-Do lists</title></html>')
     return render(request, 'home.html', {
-                'new_item_text': item.text
+                'new_item_text': new_item_text
             })
