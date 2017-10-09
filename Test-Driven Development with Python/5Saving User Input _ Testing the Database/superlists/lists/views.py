e91a2d9e2e15c2e9de181b84c5a89bce27f137ec
greatabel <myreceiver2for2github@gmail.com>
2017-10-09 14:31:02 +0800

    pass test_displays_all_list_items

--- Test-Driven Development with Python/5Saving User Input _ Testing the Database/superlists/lists/views.py
+++ Test-Driven Development with Python/5Saving User Input _ Testing the Database/superlists/lists/views.py
@@ -6,13 +6,7 @@ from lists.models import Item
 # Create your views here.
 def home_page(request):
     if request.method == 'POST':
-        new_item_text = request.POST['item_text']
-        Item.objects.create(text=new_item_text)
+        Item.objects.create(text=request.POST['item_text'])
         return redirect('/')
-    else:
-        new_item_text = ''
-    # item = Item()
-    # item.text = request.POST.get('item_text', '')
-    # item.save()
-    # return HttpResponse('<html><title>To-Do lists</title></html>')
-    return render(request, 'home.html')
+    items = Item.objects.all()
+    return render(request, 'home.html', {'items': items})
