e7eba1160a4c025fb71e1a928311f870b34eb1c4
greatabel <myreceiver2for2github@gmail.com>
2017-10-10 00:23:20 +0800

    7Working Incrementally/

---
+++ Test-Driven Development with Python/7Working Incrementally/superlists/lists/tests.py
@@ -0,0 +1,61 @@
+from django.urls import resolve
+from django.test import TestCase
+from django.http import HttpRequest
+from django.template.loader import render_to_string
+from lists.views import home_page
+from lists.models import Item
+
+# Create your tests here.
+# class SmokeTest(TestCase):
+
+#     def test_bad_maths(self):
+#         self.assertEqual(1 + 1, 3)
+class HomePageTest(TestCase):
+
+    def test_only_saves_items_when_necessary(self): 
+        self.client.get('/')
+        self.assertEqual(Item.objects.count(), 0)
+
+    def test_uses_home_template(self):
+        response = self.client.get('/')
+        html = response.content.decode('utf8')
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+    def test_redirects_after_POST(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})       
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+
+    def test_displays_all_list_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        response = self.client.get('/')
+        self.assertIn('itemey 1', response.content.decode())
+        self.assertIn('itemey 2', response.content.decode())
+        
+
+class ItemModelTest(TestCase):
+
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.save()
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(second_saved_item.text, 'Item the second')
---
+++ Test-Driven Development with Python/7Working Incrementally/superlists/lists/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import redirect, render
+from django.http import HttpResponse
+from lists.models import Item
+
+
+# Create your views here.
+def home_page(request):
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST['item_text'])
+        return redirect('/')
+    items = Item.objects.all()
+    return render(request, 'home.html', {'items': items})
