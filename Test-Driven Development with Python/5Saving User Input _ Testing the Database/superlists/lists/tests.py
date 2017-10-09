d2557d7943c7f10048c47722d7a0cbf542fca967
greatabel <myreceiver2for2github@gmail.com>
2017-10-09 14:24:03 +0800

    add test_displays_all_list_items in tests.py

--- Test-Driven Development with Python/5Saving User Input _ Testing the Database/superlists/lists/tests.py
+++ Test-Driven Development with Python/5Saving User Input _ Testing the Database/superlists/lists/tests.py
@@ -32,6 +32,14 @@ class HomePageTest(TestCase):
         response = self.client.post('/', data={'item_text': 'A new list item'})       
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response['location'], '/')
+
+    def test_displays_all_list_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        response = self.client.get('/')
+        self.assertIn('itemey 1', response.content.decode())
+        self.assertIn('itemey 2', response.content.decode())
         
 
 class ItemModelTest(TestCase):
