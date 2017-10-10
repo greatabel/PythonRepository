85f42a25eaf7ccb390601cb99014e8a34c22bf02
greatabel <myreceiver2for2github@gmail.com>
2017-10-10 17:26:33 +0800

    test_multiple_users_can_start_lists_at_different_urls

--- Test-Driven Development with Python/7Working Incrementally/superlists/functional_tests/tests.py
+++ Test-Driven Development with Python/7Working Incrementally/superlists/functional_tests/tests.py
@@ -32,5 +32,40 @@ class NewVisitorTest(LiveServerTestCase):
     def test_can_start_a_list_for_one_user(self):
         self.browser.get(self.live_server_url)
 
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+        
+        # She is invited to enter a to-do item straight away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+        # 为解决这个send_keys 不work
+        # pip3.5 install --upgrade selenium
+        # brew upgrade geckodriver
+
+        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
+        # time.sleep(1)
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
+        self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
+
     def test_multiple_users_can_start_lists_at_different_urls(self):
         self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
+
+        edit_list_url = self.browser.current_url
+        self.assertRegex(edit_list_url, '/lists/.+')
+
+
