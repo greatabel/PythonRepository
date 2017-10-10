d1faa02ba52a15587b5b3e5f084ba5f4d01286fb
greatabel <myreceiver2for2github@gmail.com>
2017-10-10 17:15:22 +0800

    Ensuring We Have a Regression Test

--- Test-Driven Development with Python/7Working Incrementally/superlists/functional_tests/tests.py
+++ Test-Driven Development with Python/7Working Incrementally/superlists/functional_tests/tests.py
@@ -29,40 +29,8 @@ class NewVisitorTest(LiveServerTestCase):
                     raise e
                 time.sleep(0.5)
 
-    def test_can_start_a_list_and_retrieve_it_later(self):
-
+    def test_can_start_a_list_for_one_user(self):
         self.browser.get(self.live_server_url)
 
-        self.assertIn('To-Do', self.browser.title)
-        header_text = self.browser.find_element_by_tag_name('h1').text
-        self.assertIn('To-Do', header_text)
-        
-        # She is invited to enter a to-do item straight away
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertEqual(
-            inputbox.get_attribute('placeholder'),
-            'Enter a to-do item'
-        )
-        # 为解决这个send_keys 不work
-        # pip3.5 install --upgrade selenium
-        # brew upgrade geckodriver
-
-        inputbox.send_keys('Buy peacock feathers')
-        inputbox.send_keys(Keys.ENTER)
-        # time.sleep(1)
-        self.wait_for_row_in_list_table('1: Buy peacock feathers')
-
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        inputbox.send_keys('Use peacock feathers to make a fly')
-        inputbox.send_keys(Keys.ENTER)
-        # time.sleep(1)
-
-        self.wait_for_row_in_list_table('1: Buy peacock feathers')
-        self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
-
-        self.fail('Finish the test!')
-
-        # She is invited to enter
-
-# if __name__ == '__main__':
-#     unittest.main(warnings='ignore')
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+        self.browser.get(self.live_server_url)
