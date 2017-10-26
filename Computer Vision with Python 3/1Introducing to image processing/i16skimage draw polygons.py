bbe807ce7ec26ef296e265eb2301cb20ca6a943d
greatabel <myreceiver2for2github@gmail.com>
2017-10-26 16:30:09 +0800

    i16skimage draw polygons.py

--- Computer Vision with Python 3/1Introducing to image processing/i16skimage draw polygons.py
+++ Computer Vision with Python 3/1Introducing to image processing/i16skimage draw polygons.py
@@ -6,7 +6,9 @@ img = np.zeros((200, 200), dtype=np.uint8)
 # x, y = draw.circle(100, 100, 50)
 
 # x, y = draw.ellipse(100, 100, 20, 40)
-x, y = draw.ellipse(100, 100, 20, 40)
+r = np.array([10, 25, 80, 50])
+c = np.array([10, 60, 40, 10])
+x, y = draw.polygon(r, c)
 
 # img[x, y] = 1
 # io.imshow(img)
