26d6d9bfb5ff6fa814a4518325b7fd2289de4bd4
greatabel <myreceiver2for2github@gmail.com>
2017-10-30 15:08:44 +0800

    i1logistic regression geometric.py

---
+++ Computer Vision with Python 3/5Integrating Machine Learning with Computer Vision/i1logistic regression geometric.py
@@ -0,0 +1,21 @@
+# https://florianhartl.com/logistic-regression-geometric-intuition.html
+
+import sklearn.linear_model
+# import sklearn.svm
+import numpy as np
+
+import plotly.plotly as py
+import plotly.graph_objs as go
+
+def np_r_demo():
+    V = np.array([1,2,3,4,5,6 ])
+    Y = np.array([7,8,9,10,11,12])
+    C = np.r_[V[0:2],Y[0],V[3],Y[1:3],V[4:],Y[4:]]
+    print(C)
+# np_r_demo()
+
+# data
+np.random.seed(1)
+
+X = np.r_[np.random.randn(20, 2), np.random.randn(20, 2) + [4, 4]]
+# print(X, X.shape)
