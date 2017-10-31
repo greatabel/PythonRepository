33ce1cec5840fbc4238b8b49d9d7298ef3fb8769
greatabel <myreceiver2for2github@gmail.com>
2017-10-31 14:32:59 +0800

    demo

--- Computer Vision with Python 3/5Integrating Machine Learning with Computer Vision/d4multivariate_normal demo.py
+++ Computer Vision with Python 3/5Integrating Machine Learning with Computer Vision/d4multivariate_normal demo.py
@@ -1,3 +1,4 @@
+# https://docs.scipy.org/doc/numpy-1.13.0/reference/generated/numpy.random.multivariate_normal.html
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -5,9 +6,20 @@ import matplotlib.pyplot as plt
 mean = [0, 0]
 cov = [[1, 0], [0, 100]]  # diagonal covariance
 
-
+#依据指定的均值和协方差生成数据
 x, y = np.random.multivariate_normal(mean, cov, 500).T
-print(x,'#'*10, y,'#'*20, x.shape, y.shape)
+
+print('x.shape, y.shape>', x.shape, y.shape)
 plt.plot(x, y, 'x')
 plt.axis('equal')
 plt.show()
+
+########################
+a = np.array([1, 2, 3])
+b = np.array([2, 3, 4])
+v = np.vstack((a,b))
+h = np.hstack((a,b))
+print(a, '#', b, 'np.vstack((a,b)=\n', v,
+        'np.hstack((a,b))=\n', h)
+
+########################
--- Computer Vision with Python 3/5Integrating Machine Learning with Computer Vision/d5Logistic Regression scratch.py
+++ Computer Vision with Python 3/5Integrating Machine Learning with Computer Vision/d5Logistic Regression scratch.py
@@ -3,21 +3,30 @@
 # %EF%BC%88%E9%80%BB%E8%BE%91%E5%9B%9E%E5%BD%92%EF%BC%89/
 
 # https://beckernick.github.io/logistic-regression-from-scratch/
-
+# https://datascienceplus.com/building-a-logistic-regression-in-python-step-by-step/
 import numpy as np
 import matplotlib.pyplot as plt
 # Generating Data
 
 np.random.seed(0)
-num_observations = 5000
+num_observations = 5
 
 x1 = np.random.multivariate_normal([0, 0], [[1, .75],[.75, 1]], num_observations)
 x2 = np.random.multivariate_normal([1, 4], [[1, .75],[.75, 1]], num_observations)
-print(x1.shape, x2.shape)
+
 simulated_separableish_features = np.vstack((x1, x2)).astype(np.float32)
+
 simulated_labels = np.hstack((np.zeros(num_observations),
                               np.ones(num_observations)))
 
+# print('x1.shape, x2.shape >', x1.shape, x2.shape)
+# print('simulated_separableish_features.shape>', simulated_separableish_features.shape)
+# print('simulated_labels.shape>', simulated_labels.shape)
+# print('x1, x2 >', x1, x2)
+# print('simulated_separableish_features>', simulated_separableish_features)
+# print('simulated_labels>', simulated_labels)
+# print('simulated_separableish_features[:, 0]>', simulated_separableish_features[:, 0])
+# print('simulated_separableish_features[:, 1]>', simulated_separableish_features[:, 1])
 
 plt.figure(figsize=(12,8))
 plt.scatter(simulated_separableish_features[:, 0], simulated_separableish_features[:, 1],
