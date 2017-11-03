1911ff8140bd644ea7c72940f245ed781cd84f8c
greatabel <myreceiver2for2github@gmail.com>
2017-11-03 14:36:40 +0800

    i0mlp.py implement

--- Computer Vision with Python 3/6Image Classification Using Neural Networks/i0mlp.py
+++ Computer Vision with Python 3/6Image Classification Using Neural Networks/i0mlp.py
@@ -9,8 +9,16 @@ mnist = fetch_mldata('MNIST original')
 images = mnist.data
 labels = mnist.target
 
-print('len(images)=', len(images))
+# print('len(images)=', len(images))
 
 # print(images[0],'\n')
 images = normalize(images, norm='l2')
 # print(images[0])
+images_train, images_test, labels_train, labels_test =\
+    train_test_split(images, labels, test_size=0.25, random_state=17)
+
+nn = MLPClassifier(hidden_layer_sizes=(200), max_iter=20, solver='sgd',
+                    learning_rate_init=0.001, verbose=True)
+
+nn.fit(images_train, labels_train)
+print('Network Performance: %f' % nn.score(images_test, labels_test))
