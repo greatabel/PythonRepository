ded2ce721576d9d2f42cfc99bed1b7cf236288f3
greatabel <myreceiver2for2github@gmail.com>
2017-11-15 17:28:45 +0800

    comment out

--- AbelDemo/luminagic/SNGenerator/combine_img.py
+++ AbelDemo/luminagic/SNGenerator/combine_img.py
@@ -1,7 +1,13 @@
 import os
 from PIL import Image
-
+import glob
 # https://gist.github.com/glombard/7cd166e311992a828675
+
+
+# mylist = sorted(glob.glob("sns/*.jpg"))
+# print(mylist)
+# sorted(mylist, key=lambda name: int(name[10:17]))
+# print(mylist)
 
 files =  [
     'sns/M0BGDK15000101PLOWCHD52329b30.jpg', 
@@ -18,15 +24,16 @@ files =  [
     'sns/M0BGDK15000801YG5EVA4ZK1b3569.jpg'
     ]
 
-result = Image.new("RGB", (1200, 1600))
+scale = 400
+result = Image.new("RGB", (scale*3, scale*4))
 
 for index, file in enumerate(files):
   path = os.path.expanduser(file)
   img = Image.open(path)
-  img.thumbnail((400, 400), Image.ANTIALIAS)
+  img.thumbnail((scale, scale), Image.ANTIALIAS)
   print('index=', index)
-  x = index // 4 * 400
-  y = index % 4 * 400
+  x = index // 4 * scale
+  y = index % 4 * scale
   w, h = img.size
   print('pos {0},{1} size {2},{3}'.format(x, y, w, h))
   result.paste(img, (x, y, x + w, y + h))
