83eebc7e48e99a364bc1e3606b7fbf3ca224accb
greatabel <myreceiver2for2github@gmail.com>
2017-11-16 11:27:00 +0800

    overlay.py

---
+++ AbelDemo/luminagic/SNGenerator/overlay.py
@@ -0,0 +1,13 @@
+import cv2
+s_img = cv2.imread("sns/M0BGDK15000101PLOWCHD52329b30.jpg")
+l_img = cv2.imread("sns/bg.jpg")
+x_offset=90
+y_offset=180
+l_img[y_offset:y_offset+s_img.shape[0], x_offset:x_offset+s_img.shape[1]] = s_img
+
+cv2.imwrite("sns/test.jpg", l_img)
+cv2.imshow("l_img", l_img)
+
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
