781adaec70f591c2d916cef6d1f626c9bd09c7f6
greatabel <myreceiver2for2github@gmail.com>
2017-11-19 15:38:55 +0800

    i1class.py

--- Function Programming in Python/ch2Callables/i0def.py
+++ Function Programming in Python/ch2Callables/i0def.py
@@ -5,3 +5,11 @@ hello2 = lambda name: print("Hello", name)
 
 hello1('David')
 hello2('David')
+
+
+print(hello1.__qualname__)
+print(hello2.__qualname__)
+hello3 = hello2
+print(hello3.__qualname__)
+hello3.__qualname__ = 'hello3'
+print(hello3.__qualname__)
