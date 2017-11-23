b902959fa810d06fe7e53891cb0262730aaac49e
greatabel <myreceiver2for2github@gmail.com>
2017-11-23 17:22:12 +0800

    i13sobel_edge_detection.py

---
+++ Computer Vision with Python 3/7Introduction to Computer Vision using OpenCV/i13sobel_edge_detection.py
@@ -0,0 +1,12 @@
+import cv2
+img = cv2.imread("images/image.jpg")
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+x_edges = cv2.Sobel(gray,-1,1,0,ksize=5)
+# cv2.imwrite("sobel_edges_x.jpg", x_edges)
+y_edges = cv2.Sobel(gray,-1,0,1,ksize=5)
+# cv2.imwrite("sobel_edges_y.jpg", y_edges)
+cv2.imshow("xedges", x_edges)
+cv2.imshow("yedges", y_edges)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
