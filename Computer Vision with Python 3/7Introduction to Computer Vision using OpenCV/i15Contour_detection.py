68fd6c9c5b7aa638c099c3f4a109d5e8dd540823
greatabel <myreceiver2for2github@gmail.com>
2017-11-23 17:27:10 +0800

    i15Contour_detection.py

---
+++ Computer Vision with Python 3/7Introduction to Computer Vision using OpenCV/i15Contour_detection.py
@@ -0,0 +1,12 @@
+import cv2
+img = cv2.imread('images/image.jpg')
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+thresh_img = cv2.threshold(gray,127,255,0)
+im, contours, hierarchy = cv2.findContours(thresh_img[1],cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+cv2.drawContours(img, contours, -1, (255,0,0), 3)
+# cv2.imwrite("contours.jpg", img)
+cv2.imshow("contours", img)
+
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
