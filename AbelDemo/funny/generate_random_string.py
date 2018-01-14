22af86feb3374ad4f8e094526b78f60fe8f76f4c
greatabel <myreceiver2for2github@gmail.com>
2018-01-14 23:17:45 +0800

    change

--- AbelDemo/funny/generate_random_string.py
+++ AbelDemo/funny/generate_random_string.py
@@ -3,7 +3,11 @@ import random
  
 characters = string.ascii_uppercase + string.digits + string.ascii_lowercase
  
+digits = string.digits
 def id_generator(size=6, chars=characters ):
     return ''.join(random.choice(chars) for _ in range(size))
 
-print(id_generator(30))
+print('30位数字字母', id_generator(30))
+
+print('50位数字字母', id_generator(50))
+print('digits(20纯数字)=', id_generator(20, digits))
