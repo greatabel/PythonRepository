6e50949f1eda7f45f96c85592abf374746559f38
greatabel <myreceiver2for2github@gmail.com>
2018-01-23 20:56:59 +0800

    i4thread_synchronization_lock_rlock.py 4 situations

--- 09Python Parallel Programming Cookbook/2Thread-based Parallelism/i4thread_synchronization_lock_rlock.py
+++ 09Python Parallel Programming Cookbook/2Thread-based Parallelism/i4thread_synchronization_lock_rlock.py
@@ -11,13 +11,25 @@ def increment_with_lock():
 def decrement_with_lock():
     print("decrement_with_lock")
 
+def increment_without_lock():
+    print("### increment_without_lock")
+
+ 
+def decrement_without_lock():
+    print("@@@ decrement_without_lock")
 
 if __name__ == "__main__":
     t1 = threading.Thread(target = increment_with_lock)
     t2 = threading.Thread(target = decrement_with_lock)
+    t3 = threading.Thread(target = increment_without_lock)
+    t4 = threading.Thread(target = decrement_without_lock)
 
     t1.start()
     t2.start()
+    t3.start()
+    t4.start()
 
     t1.join()
     t2.join()
+    t3.join()
+    t4.join()
