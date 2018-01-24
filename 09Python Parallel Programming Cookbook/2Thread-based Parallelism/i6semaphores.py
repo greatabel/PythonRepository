43edbf719e35669b3ec928afeb5a979bebcf351e
greatabel <myreceiver2for2github@gmail.com>
2018-01-24 10:45:15 +0800

    i6semaphores.py update

--- 09Python Parallel Programming Cookbook/2Thread-based Parallelism/i6semaphores.py
+++ 09Python Parallel Programming Cookbook/2Thread-based Parallelism/i6semaphores.py
@@ -4,6 +4,22 @@ import random
 
 semaphore = threading.Semaphore(0)
 
+def consumer():
+    print("consumer is waiting")
+
+def producer():
+    global item
+    time.sleep(1.5)
+    item = random.randint(0, 100)
+    print ("producer notify : producted item number %s" %item)
+
 if __name__ == '__main__':
     for i in range (5):
-        print(i)
+        t1 = threading.Thread(target=producer)
+        t2 = threading.Thread(target=consumer)
+        t1.start()
+        t2.start()
+        t1.join()
+        t2.join()
+
+    print ("main program terminated")
