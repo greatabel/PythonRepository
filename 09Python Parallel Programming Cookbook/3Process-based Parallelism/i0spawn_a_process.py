d6ca07fe219200595a7760ca00188591de033ae0
greatabel <myreceiver2for2github@gmail.com>
2018-01-27 19:32:43 +0800

    i0spawn_a_process.py

---
+++ 09Python Parallel Programming Cookbook/3Process-based Parallelism/i0spawn_a_process.py
@@ -0,0 +1,13 @@
+import multiprocessing
+
+def function(i):
+    print ('called function in process: %s' %i)
+    return
+
+if __name__ == '__main__':
+    Process_jobs = []
+    for i in range(5):
+        p = multiprocessing.Process(target=function, args=(i,))
+        Process_jobs.append(p)
+        p.start()
+        p.join()
