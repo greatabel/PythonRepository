5d18ab10db890e3f84b86ebf778a19f8b698ee6a
greatabel <myreceiver2for2github@gmail.com>
2018-02-05 14:53:55 +0800

    init PyAct

--- AbelDemo/funny/generate_random_string.py
+++ AbelDemo/funny/generate_random_string.py
@@ -8,6 +8,9 @@ def id_generator(size=6, chars=characters ):
     return ''.join(random.choice(chars) for _ in range(size))
 
 print('30位数字字母', id_generator(30))
-
 print('50位数字字母', id_generator(50))
 print('digits(20纯数字)=', id_generator(20, digits))
+
+print('-'*20)
+for i in range(0, 5):
+    print('20位数字字母:', id_generator(20))
---
+++ PyAct/resource.md
@@ -0,0 +1,1 @@
+http://www.quietaffiliate.com/free-first-name-and-last-name-databases-csv-and-sql/
