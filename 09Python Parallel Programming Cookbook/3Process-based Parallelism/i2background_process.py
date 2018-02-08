b155f2a678ac457fc008e503177355694b4e68ae
greatabel <myreceiver2for2github@gmail.com>
2018-02-08 13:49:48 +0800

    i2background_process.py

---
+++ 09Python Parallel Programming Cookbook/3Process-based Parallelism/i2background_process.py
@@ -0,0 +1,22 @@
+import multiprocessing
+import time
+
+def foo():
+    name = multiprocessing.current_process().name
+    print ("Starting %s \n" %name)
+    time.sleep(3)
+    print ("Exiting %s \n" %name)
+
+if __name__ == '__main__':
+    background_process = multiprocessing.Process\
+                         (name='background_process0', \
+                          target=foo)
+    background_process.daemon = True
+
+    NO_background_process = multiprocessing.Process\
+                            (name='NO_background_process0',\
+                             target=foo)    
+    NO_background_process.daemon = False
+
+    background_process.start()
+    NO_background_process.start()
