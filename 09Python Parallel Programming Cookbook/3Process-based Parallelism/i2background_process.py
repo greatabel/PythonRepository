eb06a1bd8ff8c5fe078ed13d83d42b36dd66ca48
greatabel <myreceiver2for2github@gmail.com>
2018-02-08 16:00:03 +0800

    i2background_process.py make main process exit later

--- 09Python Parallel Programming Cookbook/3Process-based Parallelism/i2background_process.py
+++ 09Python Parallel Programming Cookbook/3Process-based Parallelism/i2background_process.py
@@ -4,19 +4,22 @@ import time
 def foo():
     name = multiprocessing.current_process().name
     print ("Starting %s \n" %name)
-    time.sleep(3)
+    time.sleep(1)
+    file = open(name + ".txt", "w") 
     print ("Exiting %s \n" %name)
 
 if __name__ == '__main__':
     background_process = multiprocessing.Process\
-                         (name='background_process0', \
+                         (name='0background_process', \
                           target=foo)
     background_process.daemon = True
 
     NO_background_process = multiprocessing.Process\
-                            (name='NO_background_process0',\
+                            (name='0NO_background_process0',\
                              target=foo)    
     NO_background_process.daemon = False
 
     background_process.start()
     NO_background_process.start()
+    time.sleep(3)
+    print('主进程退出会杀死所有daemo进程，所以我迟一点!')
