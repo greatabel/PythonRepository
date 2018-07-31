5efa3cb58468ab3b9d53cbcd26b62e55bcd7d003
greatabel <myreceiver2for2github@gmail.com>
2018-07-31 10:53:02 +0800

    filter poems

--- AbelDemo/luminagic/poem_scrapy/i0poem_scrapy.py
+++ AbelDemo/luminagic/poem_scrapy/i0poem_scrapy.py
@@ -8,18 +8,27 @@ def csv_printer_from_localfile(filename, directory='./'):
     with open(os.path.join(directory, filename), newline='') as csvfile:
         reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
         i = 0
+        peoms = []
         for row in reader:
             if row:
                 if row[0] is not None and row[0] != "\"":
+                    # 去掉（ 和 之后的脏字符串
+                    row[0] = row[0].split('(', 1)[0]
+                    # 去掉。造成的错误断句
                     sentences = row[0].replace('。', '，').split('，')
-                    print(row[0], '#'*10, sentences)
-                    i += 1
+                    # 去掉。造成的最后一个空句
+                    if '' in sentences:
+                        sentences.remove('')
+                    # 去掉不是每句长度一致的词
+                    if all(len(x) == len(sentences[0]) for x in sentences):
+                        print(row[0], '#'*10, sentences, len(sentences))
+                        i += 1
         print(i)
 
 def csv_writer_book_to_local(books, last_readen_order, filename):
     with open(filename, 'a') as csvfile:
-        fieldnames = ['ReadenOrder', 'EntryDate','Category','BookName']
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        writer = csv.DictWriter(csvfile)
         for book in books:
             writer.writerow({'ReadenOrder': last_readen_order + 1 + book.readen_order, 'EntryDate': book.been_read_date,
                              'Category':book.category, 'BookName':book.name})
