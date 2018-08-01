752501b27886b625ff49ec66a80e1f5255585fb2
greatabel <myreceiver2for2github@gmail.com>
2018-08-01 12:52:58 +0800

    sort by sentiment

--- AbelDemo/luminagic/poem_scrapy/i0poem_scrapy.py
+++ AbelDemo/luminagic/poem_scrapy/i0poem_scrapy.py
@@ -1,6 +1,7 @@
 import os
 import scrapy
 import csv
+import json
 
 print(scrapy.version_info)
 
@@ -20,17 +21,51 @@ def csv_printer_from_localfile(filename, directory='./'):
                     if '' in sentences:
                         sentences.remove('')
                     # 去掉不是每句长度一致的词
-                    if all(len(x) == len(sentences[0]) for x in sentences):
+                    if all(len(x) == len(sentences[0]) for x in sentences) and \
+                    (len(sentences) == 4 or len(sentences) == 8) and \
+                        row[0] != '' and row[0]!= 'article' and \
+                        row[0] not in peoms:
                         print(row[0], '#'*10, sentences, len(sentences))
                         i += 1
+
+                        peoms.append(row[0])
         print(i)
+        # 根据诗句长度排序
+        peoms.sort(key=len)
+        print(peoms[0:20])
+        peoms.sort(key=by_feeling_key,reverse=True)
+        print(peoms[0:20])
+        return peoms
 
-def csv_writer_book_to_local(books, last_readen_order, filename):
-    with open(filename, 'a') as csvfile:
+# def csv_writer_book_to_local(peoms, filename):
+#     with open(filename, 'a') as csvfile:
+#         writer = csv.DictWriter(csvfile, fieldnames=['poem'])
+#         writer.writeheader()
+#         for poem in peoms:
+#             writer.writerow({'poem': poem})
 
-        writer = csv.DictWriter(csvfile)
-        for book in books:
-            writer.writerow({'ReadenOrder': last_readen_order + 1 + book.readen_order, 'EntryDate': book.been_read_date,
-                             'Category':book.category, 'BookName':book.name})
+def write_to_json(poems, filename):
+    with open(filename, 'a') as outfile:
+        json.dump(poems, outfile, ensure_ascii=False)
 
-csv_printer_from_localfile('poems.csv', 'peom/')
+def by_feeling_key(poem):
+    sad = ['鸣', '薄命', '胸', '情人', '怜']
+    happy = ['春', '美', '山', '田', '树', '鸟']
+    count = 0
+    for s in sad:
+        if s in poem:
+            count -= 4
+    for h in happy:
+        if h in poem:
+            count += 1
+    sentences = poem.replace('。', '，').split('，')
+    if len(sentences) == 4:
+        count += 2
+
+    return count
+
+
+
+poems = csv_printer_from_localfile('poems.csv', 'peom/')
+# csv_writer_book_to_local(poems, 'processed.csv')
+write_to_json(poems, 'poems.json')
