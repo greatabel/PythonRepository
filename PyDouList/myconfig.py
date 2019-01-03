f0d52ea08f7b208ccdb0b684eff03f208a107ae7
greatabel <myreceiver2for2github@gmail.com>
2019-01-03 23:13:13 +0800

    pydoulist config

--- PyDouList/myconfig.py
+++ PyDouList/myconfig.py
@@ -13,7 +13,7 @@ doulist_prex = 'https://www.douban.com/doulist/'
 
 scrawler_pagelimit = 1
 scrawler_pagelimit_doulist = 2
-last_async_time = '2018-12-26 10:33:14'
+last_async_time = '2019-01-03 00:27:14'
 # I ignore @@@ started files in .gitignore
 # filename01 = '@@@01my_all_doulist#'+datetime.datetime.today().strftime('%Y-%m-%d')
 
