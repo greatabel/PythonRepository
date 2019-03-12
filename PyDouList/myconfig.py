9a5e6d939217fb85e5c7b8a711c1dc6259dda8a1
greatabel <myreceiver2for2github@gmail.com>
2019-03-12 11:26:25 +0800

    change date of ansync

--- PyDouList/myconfig.py
+++ PyDouList/myconfig.py
@@ -13,7 +13,7 @@ doulist_prex = 'https://www.douban.com/doulist/'
 
 scrawler_pagelimit = 1
 scrawler_pagelimit_doulist = 2
-last_async_time = '2019-02-28 09:42:14'
+last_async_time = '2019-03-12 09:42:14'
 # I ignore @@@ started files in .gitignore
 # filename01 = '@@@01my_all_doulist#'+datetime.datetime.today().strftime('%Y-%m-%d')
 
