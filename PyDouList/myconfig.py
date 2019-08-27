4bc7d572c63fa90cffd49435abc15eedb9412c90
greatabel <myreceiver2for2github@gmail.com>
2019-08-27 12:15:50 +0800

    last book async from luminagic work place

--- PyDouList/myconfig.py
+++ PyDouList/myconfig.py
@@ -13,7 +13,7 @@ doulist_prex = 'https://www.douban.com/doulist/'
 
 scrawler_pagelimit = 1
 scrawler_pagelimit_doulist = 2
-last_async_time = '2019-08-05 01:17:14'
+last_async_time = '2019-08-27 12:15:14'
 # I ignore @@@ started files in .gitignore
 # filename01 = '@@@01my_all_doulist#'+datetime.datetime.today().strftime('%Y-%m-%d')
 
