961faa68b236cf53fd2b690ca653453a1c708375
greatabel <myreceiver2for2github@gmail.com>
2019-08-28 23:40:14 +0800

    python interview

---
+++ 12PythonAlgorithmInterview/i0reverse_linklist.py
@@ -0,0 +1,32 @@
+'''
+给定一个带头节点的单链表，将其逆序
+
+
+#----------------------------#
+
+
+
+'''
+
+
+
+
+import time
+from termcolor import colored
+
+
+def main_process():
+    print(colored('mycount=', 'red'), 'results')
+
+if __name__ == "__main__":
+    tic = time.process_time()
+    
+    main_process()
+
+    toc = time.process_time()
+    print("time=",toc - tic)
+
+
+
+
+
