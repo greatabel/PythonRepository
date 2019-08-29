a53523ba2df629de586cd7e18b9bb7b0357ca1c9
greatabel <myreceiver2for2github@gmail.com>
2019-08-29 23:08:28 +0800

    try

--- 12PythonAlgorithmInterview/i0reverse_linklist.py
+++ 12PythonAlgorithmInterview/i0reverse_linklist.py
@@ -14,8 +14,20 @@
 import time
 from termcolor import colored
 
+class Node(object):
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+def reverse_list_by_constructive(head):
+    new_head = None
+    while head:
+        new_head = Node(head.value, new_head)
+        head = head.next
+    return new_head
 
 def main_process():
+    
     print(colored('mycount=', 'red'), 'results')
 
 if __name__ == "__main__":
