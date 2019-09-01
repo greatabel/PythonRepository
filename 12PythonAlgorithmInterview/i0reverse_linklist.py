ac4c16d5569e8757bd13f521d8872c3745ca1842
greatabel <myreceiver2for2github@gmail.com>
2019-09-01 10:59:51 +0800

    Node & LinkedList sturcture

--- 12PythonAlgorithmInterview/i0reverse_linklist.py
+++ 12PythonAlgorithmInterview/i0reverse_linklist.py
@@ -1,6 +1,6 @@
 '''
-给定一个带头节点的单链表，将其逆序
-
+1.1 给定一个带头节点的单链表，将其逆序
+即如果单链表原来为 head -> 1 -> 2 -> 3 逆序后变成 head -> 3 -> 2 -> 1
 
 #----------------------------#
 
@@ -15,26 +15,37 @@ import time
 from termcolor import colored
 
 
+class Node:
+    def __init__(self, data, nextnode=None):
+        self.data = data
+        self.nextnode = nextnode
+
+
 class LinkedList:
-    def __init__(self, value, next=None):
-        self.value = value
-        self.next = next
+    def __init__(self, head=None):
+        self.head = head
+        self.size = 0
 
-    def __repr__(self):
-        return 'LinkedList({}, {})'.format(self.value, repr(self.next))
+    def add_node(self, data):
+        new_node = Node(data, self.head)
+        self.head = new_node
+        self.size += 1
 
+    def print_node(self):
+        curr = self.head
+        while curr:
+            print(curr.data)
+            curr = curr.nextnode
 
-def reverse(item, tail = None):
-    next = item.next
-    item.next = tail
-    if next is None:
-        return item
-    else:
-        return reverse(next, item)
 
 
 def main_process():
-    
+    linkedlist = LinkedList()
+    for i in reversed(range(1, 7+1)):        
+        linkedlist.add_node(i)
+        print('i=', i)
+
+    linkedlist.print_node()
     print(colored('mycount=', 'red'), 'results')
 
 if __name__ == "__main__":
