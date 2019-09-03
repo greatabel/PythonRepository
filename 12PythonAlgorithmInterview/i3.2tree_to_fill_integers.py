40eb30e81d68bb62fa9e7bb1d004b645450b0335
greatabel <myreceiver2for2github@gmail.com>
2019-09-03 14:31:53 +0800

    i3.2tree_to_fill_integers.py

---
+++ 12PythonAlgorithmInterview/i3.2tree_to_fill_integers.py
@@ -0,0 +1,29 @@
+'''
+
+如何把一个有序整数数组放到二叉树
+
+#----------------------------#
+
+
+
+'''
+
+import time
+from termcolor import colored
+
+
+def main_process():
+    print(colored('mycount=', 'red'), 'results')
+
+if __name__ == "__main__":
+    tic = time.process_time()
+    
+    main_process()
+
+    toc = time.process_time()
+    print("time=",toc - tic)
+
+
+
+
+
