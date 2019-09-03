e1527736c1db3084aad96c3abc2fc6f031fce11c
greatabel <myreceiver2for2github@gmail.com>
2019-09-03 11:02:20 +0800

    i2.2queue.py

---
+++ 12PythonAlgorithmInterview/i2.2queue.py
@@ -0,0 +1,34 @@
+'''
+
+实现一个队列，具有入队、出队、查看首位元素、
+查看队列大小等
+
+
+#----------------------------#
+
+思路：数组实现
+
+'''
+
+
+
+
+import time
+from termcolor import colored
+
+
+def main_process():
+    print(colored('mycount=', 'red'), 'results')
+
+if __name__ == "__main__":
+    tic = time.process_time()
+    
+    main_process()
+
+    toc = time.process_time()
+    print("time=",toc - tic)
+
+
+
+
+
