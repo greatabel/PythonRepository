ac2a0a2781f0c666df9f59c56152dc0229da8914
greatabel <myreceiver2for2github@gmail.com>
2019-09-03 23:52:33 +0800

    i6.1number_is_square.py

---
+++ 12PythonAlgorithmInterview/i6.1number_is_square.py
@@ -0,0 +1,29 @@
+'''
+
+设计一个算法，判断给定一个数n是否是某个数的平方，不能使用开平方运算
+
+#----------------------------#
+
+
+
+'''
+
+import time
+from termcolor import colored
+
+
+def main_process():
+    print(colored('mycount=', 'red'), 'results')
+
+if __name__ == "__main__":
+    tic = time.process_time()
+    
+    main_process()
+
+    toc = time.process_time()
+    print("time=",toc - tic)
+
+
+
+
+
