661bbbe67720985691c770a2c977bdba9fa59d26
greatabel <myreceiver2for2github@gmail.com>
2019-09-06 11:54:23 +0800

    thread func

--- 12PythonAlgorithmInterview/python_interview/i5multiple_threads.py
+++ 12PythonAlgorithmInterview/python_interview/i5multiple_threads.py
@@ -7,22 +7,31 @@ Python中如何实现多线程？
 
 
 '''
-
+import logging
+import threading
 import time
 from termcolor import colored
 
 
-def main_process():
-    
-    print(colored('mycount=', 'red'), 'results')
+def thread_function(name):
+    logging.info("线程 %s: starting", name)
+    time.sleep(2)
+    logging.info("线程 %s: finishing", name)
+
+
 
 if __name__ == "__main__":
-    tic = time.process_time()
-    
-    main_process()
+    format = "%(asctime)s: %(message)s"
+    logging.basicConfig(format=format, level=logging.INFO,
+                        datefmt="%H:%M:%S")
 
-    toc = time.process_time()
-    print("time=",toc - tic)
+    logging.info("主线程    : before creating thread")
+    x = threading.Thread(target=thread_function, args=(1,))
+    logging.info("主线程    : before running thread")
+    x.start()
+    logging.info("主线程    : wait for the thread to finish")
+    # x.join()
+    logging.info("主线程    : all done")
 
 
 
