c55f0d855731292c7c43683aa47a94342bc47438
greatabel <myreceiver2for2github@gmail.com>
2019-09-06 11:12:45 +0800

    func parameter

--- 12PythonAlgorithmInterview/python_interview/i2deepcopy_shadowcopy.py
+++ 12PythonAlgorithmInterview/python_interview/i2deepcopy_shadowcopy.py
@@ -17,18 +17,41 @@ def main_process():
     ans = '深拷贝就是将一个对象拷贝到另一个对象中，这意味着如果你对一个对象的拷贝做出改变时，不会影响原对象'
     print(colored('mycount=', 'red'), ans)
 
-    print(colored('--------------------', 'green'), '相关复习', colored('-'*20, 'red'))
+    print(colored('--------------------', 'green'), '可变对象/不可变对象', colored('-'*20, 'red'))
     print('可变对象：list,dict, set 不可变对象：tuple,string int float bool', )
     #  可变对象
     a = [1, 2, 3]
-    print(a)
+    print(a, 'id(a)=', id(a))
     a[1] = 20
-    print(a)
+    print(a, '修改后 id(a)=', id(a))
+    print()
     b = (1, 2, 3)
     try:
         b[1] = 200
     except:
         print('修改不可修改对象失败') 
+
+    b1 = b
+    b = (4, 5, 6)
+    print(b, b1, 'id(b),id(b1)=', id(b), id(b1))
+    print(colored('--------------------', 'green'), '可变对象/不可变对象 作为函数参数',
+             colored('-'*20, 'red'))
+    def myfunc(l):
+        l.append(1)
+    l = [1, 2, 3]
+    print('可变对象 作为函数参数', l)
+    myfunc(l)
+    print('可变对象 作为函数参数', l)
+
+    def myfunc_unchangeable_para(a):
+        a += 1
+        print('in func', a)
+    a = 2
+    print('不可变对象，作为函数参数', a)
+    myfunc_unchangeable_para(a)
+    print('不可变对象，作为函数参数', a)
+    print('结论：python中向函数传递参数只能是引用传递，表示把它的地址都传进去了，这才会带来上面的现象')
+
 
 
 
