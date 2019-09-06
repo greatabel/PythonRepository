d4e58d457a127dc337bb198559fc5188ecb7f3ac
greatabel <myreceiver2for2github@gmail.com>
2019-09-06 10:52:18 +0800

    changeable

--- 12PythonAlgorithmInterview/python_interview/i2deepcopy_shadowcopy.py
+++ 12PythonAlgorithmInterview/python_interview/i2deepcopy_shadowcopy.py
@@ -17,6 +17,21 @@ def main_process():
     ans = '深拷贝就是将一个对象拷贝到另一个对象中，这意味着如果你对一个对象的拷贝做出改变时，不会影响原对象'
     print(colored('mycount=', 'red'), ans)
 
+    print(colored('--------------------', 'green'), '相关复习', colored('-'*20, 'red'))
+    print('可变对象：list,dict, set 不可变对象：tuple,string int float bool', )
+    #  可变对象
+    a = [1, 2, 3]
+    print(a)
+    a[1] = 20
+    print(a)
+    b = (1, 2, 3)
+    try:
+        b[1] = 200
+    except:
+        print('修改不可修改对象失败') 
+
+
+
 if __name__ == "__main__":
     tic = time.process_time()
     
