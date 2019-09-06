9fed6d2f1b65fa78fdfc74597f36151dfdac69cc
greatabel <myreceiver2for2github@gmail.com>
2019-09-06 15:33:01 +0800

    inherit demo

--- 12PythonAlgorithmInterview/python_interview/i6inherit.py
+++ 12PythonAlgorithmInterview/python_interview/i6inherit.py
@@ -12,6 +12,20 @@ import time
 from termcolor import colored
 
 
+x = 0
+class fruit:
+    def __init__(self):
+        global x
+        x += 1
+        print("I'm a fruit")
+
+class citrus(fruit):
+    def __init__(self):
+        super().__init__()
+        global x
+        x += 10
+        print('I am citrus')
+
 def main_process():
     t = '解释一下Python中的继承?\
         承能让我们重新使用代码，也能更容易的创建和维护应用。Python支持如下种类的继承：\
@@ -21,6 +35,8 @@ def main_process():
         \n分层继承：多个类继承自单个基类\
         \n混合继承：两种或多种类型继承的混合'
     print(colored('mycount=', 'red'), t)
+    lime = citrus()
+    print('x=', x)
 
 if __name__ == "__main__":
     tic = time.process_time()
