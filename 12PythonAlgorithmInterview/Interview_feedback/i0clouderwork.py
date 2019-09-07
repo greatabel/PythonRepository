3a46aa322cc3ebf6dc023f25da7733d0ebc66804
greatabel <myreceiver2for2github@gmail.com>
2019-09-07 11:50:57 +0800

    HTTP Methods vs Restful_API

--- 12PythonAlgorithmInterview/Interview_feedback/i0clouderwork.py
+++ 12PythonAlgorithmInterview/Interview_feedback/i0clouderwork.py
@@ -4,6 +4,7 @@
 
 #----------------------------#
 
+https://juejin.im/entry/57bd4e6fa633bd005d482ed1
 
 
 '''
@@ -13,7 +14,14 @@ from termcolor import colored
 
 
 def main_process():
-    print(colored('mycount=', 'red'), 'results')
+    t = '''
+    1. HTTP Methods 一共有九个，分别是 GET，HEAD，POST，PUT，DELETE，TRACE，OPTIONS，CONNECT，PATCH。
+    在RESTful API 设计中，常用的有POST，GET，PUT，PATCH 和 DELETE
+
+    2. 
+
+    '''
+    print(colored('-'*20, 'red'), t)
 
 if __name__ == "__main__":
     tic = time.process_time()
