ad20cffd856613e482b792bc85e800f1eba63f5f
greatabel <myreceiver2for2github@gmail.com>
2019-09-07 08:16:26 +0800

    questions of flask"

--- 12PythonAlgorithmInterview/python_interview/i7flask_related_question.py
+++ 12PythonAlgorithmInterview/python_interview/i7flask_related_question.py
@@ -20,6 +20,12 @@ def main_process():
     Flask使用 BSD 授权。其中两个环境依赖是Werkzeug和jinja2，这意味着它不需要依赖外部库。
     正因如此，我们将其称为轻量级框架
     --------------------
+    2. flask是一个MVC模型？
+    flask是个经典的MVC框架
+    
+    3. flask中连接数据库？
+    在脚本中以用第三方库正常连接，用sql语句正常操作数据库，如mysql关系型数据库的pymsql库
+    用ORM来进行数据库连接，flask中典型的flask_sqlalchemy，已面向对象的方式进行数据库的连接与操
 
     '''
     print(colored('-'*20, 'red'), t)
