e008fd1272099ba4f3854e5599838271b6fda0ee
greatabel <myreceiver2for2github@gmail.com>
2019-09-08 10:18:48 +0800

    outer monkey

--- 12PythonAlgorithmInterview/python_interview/i11monkey_patch.py
+++ 12PythonAlgorithmInterview/python_interview/i11monkey_patch.py
@@ -19,6 +19,9 @@ class A:
     def monkey(self):
         print('Hi monkey!')
 
+## a 这个参数是没有用到的，因为func有一个参数，如果这个函数没有参数的话不能这样直接赋值
+def outer_monkey(a):
+    print('Hi, outer_monkey')
 
 def main_process():
     t = '''
@@ -32,7 +35,11 @@ def main_process():
     print('使用猴子补丁')
     A.func = A.monkey
     a.func()
-    print('其实这根本的原因在于Python语法的灵活性，方法可以像普通对象那样使用!')
+    print('其实这根本的原因在于Python语法的灵活性，方法可以像普通对象那样使用!\n')
+
+    print('方法2： 也可以这样使用')
+    A.func = outer_monkey
+    a.func()
 
 if __name__ == "__main__":
     tic = time.process_time()
