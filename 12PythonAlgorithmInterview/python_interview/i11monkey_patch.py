88c0238005c3f90fe860d168bc0ce2eeac340d76
greatabel <myreceiver2for2github@gmail.com>
2019-09-08 10:13:29 +0800

    demo of monkey patch

--- 12PythonAlgorithmInterview/python_interview/i11monkey_patch.py
+++ 12PythonAlgorithmInterview/python_interview/i11monkey_patch.py
@@ -12,12 +12,27 @@ import time
 from termcolor import colored
 
 
+class A:
+    def func(self):
+        print('Hi')
+
+    def monkey(self):
+        print('Hi monkey!')
+
+
 def main_process():
     t = '''
     Q 11. 什么是猴子补丁？
     在运行期间动态修改一个类或模块
     '''
     print(colored('-'*10, 'red'), t)
+
+    a = A()
+    a.func()
+    print('使用猴子补丁')
+    A.func = A.monkey
+    a.func()
+    print('其实这根本的原因在于Python语法的灵活性，方法可以像普通对象那样使用!')
 
 if __name__ == "__main__":
     tic = time.process_time()
