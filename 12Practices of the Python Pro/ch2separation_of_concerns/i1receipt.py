4b11ff0823e3873dd151342ca00eb0a27105dcc8
greatabel <myreceiver2for2github@gmail.com>
2020-02-22 16:47:58 +0800

    change

--- 12Practices of the Python Pro/ch2separation_of_concerns/i1receipt.py
+++ 12Practices of the Python Pro/ch2separation_of_concerns/i1receipt.py
@@ -8,5 +8,13 @@ def print_receipt():
     print(f'AFTER TAX: {add_sales_tax(total, state)}')  # <2>
 
 
+import i0sales_tax
+
+
+def print_receipt():
+    total = 10
+    locale = 'MI'
+    print(f'AFTER MILLAGE: {i0sales_tax.add_sales_tax(total, locale)}')
+
 if __name__ == "__main__":
     print_receipt()
