9ec64e40b75b4288ef376a65b80c9f13ef3886e2
greatabel <myreceiver2for2github@gmail.com>
2020-02-22 15:52:01 +0800

    i0sales_tax.py

--- 11Computer Vision with Python 3/9Video_Processing_Opencv/i0read_video.py
+++ 11Computer Vision with Python 3/9Video_Processing_Opencv/i0read_video.py
@@ -1,10 +1,12 @@
 import cv2
+import time
 
 cam = cv2.VideoCapture(0)
 
 
 while (cam.isOpened()):
     ret, frame = cam.read()
+    time.sleep(0.07)
     cv2.imshow('frame', frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
