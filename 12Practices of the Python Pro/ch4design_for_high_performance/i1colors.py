d9384d5322bf5e00492fa479e6d3c99e46f3e3e9
greatabel <myreceiver2for2github@gmail.com>
2020-02-24 16:27:42 +0800

    space

--- 12Practices of the Python Pro/ch4design_for_high_performance/i1colors.py
+++ 12Practices of the Python Pro/ch4design_for_high_performance/i1colors.py
@@ -10,3 +10,26 @@ for color in favorite_colors:
         color_counts[color] = 1
 
 print(color_counts)
+
+# Loading a single color at a time into memory
+color_counts = {}
+
+with open('i1all-favorite-colors.txt') as favorite_colors_file:
+    for color in favorite_colors_file:  # <1>
+        color = color.strip()  # <2>
+
+        if color in color_counts:
+            color_counts[color] += 1
+        else:
+            color_counts[color] = 1
+
+print(color_counts)
+
+# Using a set to store only unique colors seen
+all_colors = set()
+
+with open('i1all-favorite-colors.txt') as favorite_colors_file:
+    for line in favorite_colors_file:  # <1>
+        all_colors.add(line.strip())  # <2>
+
+print('red' in all_colors)  # <3>
