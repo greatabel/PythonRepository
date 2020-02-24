45c6a6e80db539f8b6367d0ba96d5f25b6113ea0
greatabel <myreceiver2for2github@gmail.com>
2020-02-24 11:34:52 +0800

    partial function

--- 12Practices of the Python Pro/ch3abstraction_encapsulation/i2functional.py
+++ 12Practices of the Python Pro/ch3abstraction_encapsulation/i2functional.py
@@ -10,3 +10,13 @@ square = map(lambda x: x * x, [1, 2, 3, 4, 5])
 should = reduce(lambda x, y: x and y, [True, True, False])
 evens = filter(lambda x: x % 2 == 0, [1, 2, 3, 4, 5])
 print(list(square), should, list(evens))
+
+from functools import partial
+
+def pow(x, power=1):
+    return x ** power
+
+
+square = partial(pow, power=2)
+cube = partial(pow, power=3)
+print(square(10), cube(10))
