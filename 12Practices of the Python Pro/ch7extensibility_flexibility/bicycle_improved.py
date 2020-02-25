7221f0cfbc392bda4abe344ac3122e16feda4ff2
greatabel <myreceiver2for2github@gmail.com>
2020-02-25 17:38:46 +0800

    bicycle.py

---
+++ 12Practices of the Python Pro/ch7extensibility_flexibility/bicycle_improved.py
@@ -0,0 +1,23 @@
+class Tire:
+    def __repr__(self):
+        return 'A rubber tire'
+
+
+class Frame:
+    def __repr__(self):
+        return 'An aluminum frame'
+
+class Bicycle:
+    def __init__(self):
+        self.front_tire = Tire()
+        self.back_tire = Tire()
+        self.frame = Frame()
+
+    def print_specs(self):
+        print(f'Frame: {self.frame}\n')
+        print(f'Front tire: {self.front_tire}, back tire: {self.back_tire}')
+
+
+if __name__ == '__main__': 
+    bike = Bicycle()
+    bike.print_specs()
