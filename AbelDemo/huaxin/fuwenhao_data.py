dfcde32aea63094adcbef11e31765b64d5387dba
greatabel <myreceiver2for2github@gmail.com>
2020-09-01 18:28:25 +0800

    fuwenhao

---
+++ AbelDemo/huaxin/fuwenhao_data.py
@@ -0,0 +1,46 @@
+import csv
+
+path = '/Users/abel/Desktop/2020-08-24.log'
+
+newpath = '/Users/abel/Desktop/2020-08-24new.log'
+bufsize = 65536
+
+def process(lines):
+    rows = []
+    for i in range(0,len(lines)):
+        if '两次不一致，进来...' in lines[i] and i-1 >=0 and i+2 < len(lines):
+            result = handleline(lines[i-1], lines[i], lines[i+1], lines[i+2])
+            if result is not None:
+                rows.append(result)
+    print('processing...', len(rows), ' rows')
+    return rows
+
+def handleline(line0, line1, line2, line3):
+    l0 =  line0[line0.rfind('  ')+2:]
+    if 's4=0' in l0:
+        DI = l0[l0.find(':')+1: l0.find(',')]
+
+        timediff = line2[line2.rfind('  ')+2:-1]
+
+        bag_num = line3[line3.rfind('num:')+4:-1]
+
+        # print(line0, line1, line2, line3)
+        if '警告，计算连包'  in bag_num or 'MyScreen' in timediff or 'Thread' in timediff or\
+            'Thread' in bag_num:
+            return None
+        else:
+            return (DI, timediff, bag_num)
+
+with open(path, encoding='UTF-8') as infile: 
+    while True:
+        lines = infile.readlines(bufsize)
+
+        if not lines:
+            break
+
+        rows = process(lines)
+        with open(newpath, 'a', newline='') as csvfile:
+            fieldnames = ['DI','timediff', 'bag_num']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            for row in rows:
+                writer.writerow({'DI': row[0], 'timediff': row[1], 'bag_num': row[2]})
