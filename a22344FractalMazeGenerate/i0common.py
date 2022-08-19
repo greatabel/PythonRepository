8f4bc57053a722802cc8e5630153acd4b65a0179
greatabel <myreceiver2for2github@gmail.com>
2022-08-19 22:58:07 +0800

    init a22344 add common

---
+++ a22344FractalMazeGenerate/i0common.py
@@ -0,0 +1,49 @@
+import turtle                    # import turtle library
+import time
+import sys
+from collections import deque
+
+
+# this is the class for the Maze
+class Maze(turtle.Turtle):               # define a Maze class
+    def __init__(self):
+        turtle.Turtle.__init__(self)
+        self.shape("square")            # the turtle shape
+        self.color("white")             # colour of the turtle
+        self.penup()                    # lift up the pen so it do not leave a trail
+        self.speed(0)
+
+# this is the class for the finish line - green square in the maze
+class Green(turtle.Turtle):
+    def __init__(self):
+        turtle.Turtle.__init__(self)
+        self.shape("square")
+        self.color("green")
+        self.penup()
+        self.speed(0)
+
+class Blue(turtle.Turtle):
+    def __init__(self):
+        turtle.Turtle.__init__(self)
+        self.shape("square")
+        self.color("blue")
+        self.penup()
+        self.speed(0)
+
+
+# this is the class for the yellow or turtle
+class Red(turtle.Turtle):
+    def __init__(self):
+        turtle.Turtle.__init__(self)
+        self.shape("square")
+        self.color("red")
+        self.penup()
+        self.speed(0)
+
+class Yellow(turtle.Turtle):
+    def __init__(self):
+        turtle.Turtle.__init__(self)
+        self.shape("square")
+        self.color("yellow")
+        self.penup()
+        self.speed(0)
