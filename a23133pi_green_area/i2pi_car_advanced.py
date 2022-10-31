c293f14eafe5dc49c316f25e08849e5de2b1f8c5
greatabel <myreceiver2for2github@gmail.com>
2022-10-31 17:31:34 +0800

    try 1 in a23133

--- a23133pi_green_area/i2pi_car_advanced.py
+++ a23133pi_green_area/i2pi_car_advanced.py
@@ -21,13 +21,8 @@ def main():
     global debug_mode
     # cap = cv2.VideoCapture(0)
 
-
-    font = cv2.FONT_HERSHEY_COMPLEX
-    flag = 1
-
-    global center
     if debug_mode:
-        cap = cv2.VideoCapture("simulate.mov")
+        cap = cv2.VideoCapture("simulate.mp4")
     else:
     # when use usb-camera ,only line you need to change is following:
         cap = cv2.VideoCapture(0)
@@ -44,170 +39,24 @@ def main():
         # ret, frame = cv2.VideoCapture('simulate.mov')
         ret, frame = cap.read()
         frame = cv2.GaussianBlur(frame, (5, 5), 0)
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        if flag == 0:
-            hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-            l_h = cv2.getTrackbarPos("L-H", "Trackbars")
-            l_s = cv2.getTrackbarPos("L-S", "Trackbars")
-            l_v = cv2.getTrackbarPos("L-V", "Trackbars")
-            u_h = cv2.getTrackbarPos("U-H", "Trackbars")
-            u_s = cv2.getTrackbarPos("U-S", "Trackbars")
-            u_v = cv2.getTrackbarPos("U-V", "Trackbars")
 
-            lower_black = np.array([l_h, l_s, l_v])
-            upper_black = np.array([u_h, u_s, u_v])
+        ## convert to hsv
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-            mask = cv2.inRange(hsv, lower_black, upper_black)
-            kernel = np.ones((5, 5), np.uint8)
-            mask = cv2.dilate(mask, kernel)
-            mask[np.where(mask == 0)] = 0
-            # mask = cv2.Canny(mask,50,100)
+        ## mask of green (36,25,25) ~ (86, 255,255)
+        # mask = cv2.inRange(hsv, (36, 25, 25), (86, 255,255))
+        mask = cv2.inRange(hsv, (36, 25, 25), (70, 255,255))
 
-            # Contours detection
-            if int(cv2.__version__[0]) > 3:
-                # Opencv 4.x.x
-                contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-            else:
-                # Opencv 3.x.x
-                _, contours, _ = cv2.findContours(
-                    mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE
-                )
-            oldArea = 0
-            oldCnt = 0
+        ## slice the green
+        imask = mask>0
+        green = np.zeros_like(frame, np.uint8)
+        green[imask] = frame[imask]
 
-            for cnt in contours:
-                area = cv2.contourArea(cnt)
-                approx = cv2.approxPolyDP(cnt, 0.02 * cv2.arcLength(cnt, True), True)
-                diameter = np.sqrt(4 * area / np.pi)
-                x = approx.ravel()[0]
-                y = approx.ravel()[1]
-                convvex = cv2.isContourConvex(cnt)
-                # print(len(cnt))
-                if cv2.arcLength(cnt, True) != 0:
-                    circularity = (4 * np.pi * area) / (
-                        (cv2.arcLength(cnt, True)) * (cv2.arcLength(cnt, True))
-                    )
-
-                if (
-                    area > 500
-                    and 0.5 <= float(circularity) <= 0.7
-                    and 4 < len(approx) <= 7
-                    and convvex == False
-                ):
-
-                    x1, y1, w1, h1 = cv2.boundingRect(cnt)
-
-                    x2 = x1 + w1
-                    y2 = y1 + h1
-                    if x1 < 0:
-                        x1 = 1
-                    if y1 < 0:
-                        y1 = 1
-                    if x2 > 639:
-                        x2 = 639
-                    if y2 > 479:
-                        y2 = 479
-                    gray = gray[(y1):(y2), (x1):(x2)]
-                    if gray.shape[0] != 0:
-                        if gray.shape[1] != 0:
-                            if y2 > y1:
-                                if x2 > x1:
-                                    """print(gray.shape)
-                                    cv2.imshow("ngray", gray)"""
-                                    rows = gray.shape[0]
-                                    # cv2.imshow("daire",gray)
-                                    circles = cv2.HoughCircles(
-                                        gray,
-                                        cv2.HOUGH_GRADIENT,
-                                        1,
-                                        rows / 8,
-                                        param1=50,
-                                        param2=15,
-                                        minRadius=15,
-                                        maxRadius=120,
-                                    )
-                                    if circles is not None:
-                                        circles = np.uint16(np.around(circles))
-                                        for i in circles[0, :]:
-                                            center = (i[0] + x1, i[1] + y1)
-                                            radius = i[2]
-
-                                        if area >= oldArea:
-                                            M = cv2.moments(cnt)
-                                            cX = int(M["m10"] / M["m00"])
-                                            cY = int(M["m01"] / M["m00"])
-                                            if abs(center[0] - cX) < 40:
-                                                
-                                                cv2.drawContours(
-                                                    frame, [approx], 0, (0, 0, 0), 5
-                                                )
-                                                cv2.line(
-                                                    frame,
-                                                    (cX, cY),
-                                                    (cX, cY - 60),
-                                                    (0, 255, 0),
-                                                    thickness=2,
-                                                )
-                                                cv2.line(
-                                                    frame,
-                                                    (cX, cY),
-                                                    (cX + 60, cY),
-                                                    (255, 0, 0),
-                                                    thickness=2,
-                                                )
-                                                cv2.line(
-                                                    frame,
-                                                    (cX, cY),
-                                                    (cX - 30, cY + 30),
-                                                    (0, 0, 255),
-                                                    thickness=2,
-                                                )
-                                                cv2.putText(
-                                                    frame,
-                                                    "half round",
-                                                    (x, y),
-                                                    font,
-                                                    1,
-                                                    (100, 244, 237),
-                                                    thickness=2,
-                                                )
-                                                oldArea = area
-                                                oldCnt = cnt
-
-        elif flag == 1:
-            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            img = cv2.medianBlur(gray, 37)
-            circles = cv2.HoughCircles(
-                img, cv2.HOUGH_GRADIENT, 1, 50, param1=120, param2=40
-            )
-
-            if not circles is None:
-                circles = np.uint16(np.around(circles))
-                max_r, max_i = 0, 0
-                for i in range(len(circles[:, :, 2][0])):
-                    if circles[:, :, 2][0][i] > 50 and circles[:, :, 2][0][i] > max_r:
-                        max_i = i
-                        max_r = circles[:, :, 2][0][i]
-                x, y, r = circles[:, :, :][0][max_i]
-                if y > r and x > r:
-                    square = frame[y - r : y + r, x - r : x + r]
-
-                    dominant_color = get_dominant_color(square, 2)
-                    if dominant_color[2] > 100:
-                        print("detect half circle", dominant_color, "half above is #black")
-                    elif dominant_color[0] > 80:
-                        print("detect half circle", dominant_color, "half above is #white")
-
-                    else:
-                        print("detecting")
-
-                for i in circles[0, :]:
-                    cv2.circle(frame, (i[0], i[1]), i[2], (0, 255, 0), 2)
 
         # 树梅派上可以注释掉下一行，不show出来
         if debug_mode:
-            cv2.imshow("Frame", frame)
+            cv2.imshow("Frame", green)
         key = cv2.waitKey(1)
         if key == 27:
             break
