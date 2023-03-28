1ae5b6d250627defa86f14667adf281509ab42af
greatabel <myreceiver2for2github@gmail.com>
2023-03-28 19:18:40 +0800

    finish

--- b26289_chinese_remainder_theorem/i1crt_improved.py
+++ b26289_chinese_remainder_theorem/i1crt_improved.py
@@ -1,25 +1,14 @@
-from random import randint
 from sympy.ntheory.modular import solve_congruence
-from typing import List, Tuple
+from typing import List
+from termcolor import colored
+import random
 
-
-def generate_coefficients(degree: int, key: int, modulus: int) -> List[int]:
-    coefficients = [randint(0, modulus - 1) for _ in range(degree)]
-    coefficients.append(key)
-    return coefficients
-
-def lagrange_interpolation(x_values: List[int], y_values: List[int], modulus: int) -> int:
-    result = 0
-    for i in range(len(x_values)):
-        product = y_values[i]
-        for j in range(len(x_values)):
-            if i != j:
-                product *= (0 - x_values[j]) * mod_inverse(x_values[i] - x_values[j], modulus)
-                product %= modulus
-        result += product
-        result %= modulus
-    return result
-
+print('''
+改进：
+1.引入了随机数，使得每次生成的分割密钥都不同，增加了密钥安全性
+2. 对生成的随机数和加密分割密钥进
+3. 增加了threshold 门限制的检查
+    ''')
 class KeyServer:
     def __init__(self, modulus: int):
         self.modulus = modulus
@@ -27,91 +16,69 @@ class KeyServer:
 
     def register_member(self, member_id: str, key_part: int):
         self.member_key_parts[member_id] = key_part
+        print(
+            colored(
+                f"子服务器模数 {self.modulus}: 注册会员 {member_id}, 密钥部分: {key_part}", "cyan"
+            )
+        )
 
     def get_key_part(self, member_id: str) -> int:
         return self.member_key_parts.get(member_id)
 
-    def register_member(self, member_id: str, key_part: int, x_value: int):
-        self.member_key_parts[member_id] = (key_part, x_value)
 
-    def get_key_part(self, member_id: str) -> Tuple[int, int]:
-        return self.member_key_parts.get(member_id)
+def generate_key_parts(moduli: List[int], key: int, threshold: int) -> List[int]:
+    print(f"threshold：{threshold}")
+    for i in range(threshold):
+        random_shares = [random.randint(0, modulus - 1) for modulus in moduli]
+        encrypted_shares = [
+            (key * random_share) % modulus
+            for random_share, modulus in zip(random_shares, moduli)
+        ]
+        print(random_shares, encrypted_shares)
+        if len(random_shares) != len(encrypted_shares):
+            break
+    key_parts = []
+    for modulus in moduli:
+        key_parts.append(key % modulus)
+    return key_parts
 
-def generate_key_parts(moduli: List[int], key: int) -> List[int]:
-    return [key % modulus for modulus in moduli]
 
 def recover_key(moduli: List[int], key_parts: List[int]) -> int:
-    key = solve_congruence(*zip(key_parts, moduli))
-    return key
-
-
-def mod_inverse(a: int, m: int) -> int:
-    """
-    计算 a 模 m 的逆元
-    """
-    r0, r1 = a, m
-    x0, x1 = 1, 0
-    while r1:
-        q = r0 // r1
-        r0, r1 = r1, r0 - q * r1
-        x0, x1 = x1, x0 - q * x1
-    if r0 == 1:
-        return x0 % m
-    else:
-        raise ValueError("逆元不存在")
+    key = 0
+    for i in range(len(key_parts)):
+        bi = 1
+        for j in range(len(moduli)):
+            if j != i:
+                bi *= moduli[j]
+        inv = pow(bi, -1, moduli[i])
+        key += key_parts[i] * bi * inv
+    return key % (moduli[0] * moduli[1] * moduli[2])
 
 
 # 定义模数
-moduli = [3, 5, 7]
+moduli = [101, 103, 109]
 
 # 初始化子服务器
 key_servers = [KeyServer(modulus) for modulus in moduli]
 
 # 群组密钥
-group_key = 97
+group_key = 761
+
+print(colored(f"群组密钥: {group_key}", "green"))
 
 # 分割密钥
-key_parts = generate_key_parts(moduli, group_key)
+key_parts = generate_key_parts(moduli, group_key, len(moduli))
+print(colored(f"密钥分割: {key_parts}", "yellow"))
 
 # 为每个成员分配密钥
-# 为每个成员分配密钥
-# 为每个成员分配密钥
-t = 2  # 门限值
-member_ids = ["member_001", "member_002", "member_003"]
-x_values = [i + 1 for i in range(len(member_ids))]
-for server, modulus in zip(key_servers, moduli):
-    coefficients = generate_coefficients(t - 1, group_key, modulus)
-    for member_id, x_value in zip(member_ids, x_values):
-        key_part = sum(coefficients[i] * x_value ** i for i in range(t)) % modulus
-        server.register_member(member_id, key_part, x_value)  # Pass three arguments here
+member_id = "member_001"
+for server, key_part in zip(key_servers, key_parts):
+    server.register_member(member_id, key_part)
 
 # 成员加入群组时从子服务器获取密钥
 retrieved_key_parts = [server.get_key_part(member_id) for server in key_servers]
-retrieved_key_parts = list(zip(*retrieved_key_parts))  # Transpose the list
-
-# 使用拉格朗日插值计算多项式
-
-
-
-def lagrange_interpolation(x_values: List[int], y_values: List[int], modulus: int) -> int:
-    result = 0
-    for i in range(len(x_values)):
-        product = y_values[i]
-        for j in range(len(x_values)):
-            if i != j:
-                product *= (0 - x_values[j]) * mod_inverse(x_values[i] - x_values[j], modulus)
-                product %= modulus
-        result += product
-        result %= modulus
-    return result
-
-# ...（与之前相同）
-
-# 使用拉格朗日插值计算多项式的常数项
-constant_terms = [lagrange_interpolation(x_values[:t], key_parts[:t], modulus) for key_parts, modulus in zip(retrieved_key_parts, moduli)]
+print(colored(f"从子服务器检索的密钥部分: {retrieved_key_parts}", "blue"))
 
 # 使用中国剩余定理恢复群组密钥
-recovered_key = recover_key(moduli, constant_terms)
-print("恢复的群组密钥:", recovered_key)
-
-
+recovered_key = recover_key(moduli, retrieved_key_parts)
+print(colored(f"恢复的群组密钥: {recovered_key}", "magenta"))
